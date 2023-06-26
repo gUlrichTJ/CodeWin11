2edb7a44ac42cbd545afc402668f89f2d831bc93
gUlrichTJ <gracianomathey@gmail.com>
2023-06-26 17:43:25 +0000

    Commit 3

--- CodeC++/prog1/main.cpp
+++ CodeC++/prog1/main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
+#include <vector>
+
 #define log(a) std::cout << a << std::endl;
 
 using namespace std;
 
 int increment(int& value) {
    return value++;
+}
+
+int secondSmalestValue() {
+    int const N = 10;
+    vector<int> vectorOfNumbers(N);
+
+    for (int i = 0; i < N; i++) {
+        // The user fills the vector
+
+    }
+    return 0;
 }
 
 int main()
