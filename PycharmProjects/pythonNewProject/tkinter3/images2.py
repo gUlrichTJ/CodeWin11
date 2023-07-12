619fe16530358a43d9598cf79522a121e0cc86ff
gUlrichTJ <gracianomathey@gmail.com>
2023-07-12 23:14:00 +0000

    Commit 15

---
+++ PycharmProjects/pythonNewProject/tkinter3/images2.py
@@ -0,0 +1,6 @@
+from tkinter import *
+from PIL import ImageTk, Image
+
+root = Tk()
+root.title("Codemy.com Image Viewer")
+root.iconbitmap("C:\\NetbeansProjectsWin\\PycharmProjects\\pythonNewProject\\images\\caisse2.png")
