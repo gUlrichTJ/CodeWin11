10c9774fdf42d46b5659134c840488a7c6209af3
gUlrichTJ <gracianomathey@gmail.com>
2023-07-12 23:30:43 +0000

    Commit 15.5

--- PycharmProjects/pythonNewProject/tkinter3/images2.py
+++ PycharmProjects/pythonNewProject/tkinter3/images2.py
@@ -11,7 +11,19 @@ my_img3 = ImageTk.PhotoImage(Image.open("C:\\NetbeansProjectsWin\\PycharmProject
 my_img4 = ImageTk.PhotoImage(Image.open("C:\\NetbeansProjectsWin\\PycharmProjects\\pythonNewProject\\images\\file.png"))
 my_img5 = ImageTk.PhotoImage(Image.open("C:\\NetbeansProjectsWin\\PycharmProjects\\pythonNewProject\\images\\folder.png"))
 
+image_list = [my_img1, my_img2, my_img3, my_img4, my_img5]
+
+image_list[2]
+
 my_label = Label(image=my_img1)
-my_label.pack()
+my_label.grid(row=0, column=0, columnspan=3)
+
+button_back = Button(root, text="<<")
+button_exit = Button(root, text="EXIT PROGRAM", command=root.quit)
+button_forward = Button(root, text=">>")
+
+button_back.grid(row=1, column=0)
+button_exit.grid(row=1, column=1)
+button_forward.grid(row=1, column=2)
 
 root.mainloop()
