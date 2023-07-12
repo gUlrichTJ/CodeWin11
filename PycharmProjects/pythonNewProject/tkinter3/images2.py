076c614e97058dc5789a877ea7bdf14a5e851b07
gUlrichTJ <gracianomathey@gmail.com>
2023-07-12 23:17:02 +0000

    Commit 15

--- PycharmProjects/pythonNewProject/tkinter3/images2.py
+++ PycharmProjects/pythonNewProject/tkinter3/images2.py
@@ -4,3 +4,8 @@ from PIL import ImageTk, Image
 root = Tk()
 root.title("Codemy.com Image Viewer")
 root.iconbitmap("C:\\NetbeansProjectsWin\\PycharmProjects\\pythonNewProject\\images\\caisse2.png")
+
+my_img1 = ImageTk.PhotoImage(Image.open("C:\NetbeansProjectsWin\PycharmProjects\pythonNewProject\images\cloud.png"))
+my_img2 = ImageTk.PhotoImage(Image.open("C:\NetbeansProjectsWin\PycharmProjects\pythonNewProject\images\code_c.png"))
+
+root.mainloop()
