e9db5f871509a0127c1ad6d50b0b31149ca64b2a
gUlrichTJ <gracianomathey@gmail.com>
2023-07-12 23:33:42 +0000

    Commit 15.6

--- PycharmProjects/pythonNewProject/tkinter3/images2.py
+++ PycharmProjects/pythonNewProject/tkinter3/images2.py
@@ -5,11 +5,16 @@ root = Tk()
 root.title("Codemy.com Image Viewer")
 root.iconbitmap("C:\\NetbeansProjectsWin\\PycharmProjects\\pythonNewProject\\images\\caisse2.png")
 
-my_img1 = ImageTk.PhotoImage(Image.open("C:\\NetbeansProjectsWin\\PycharmProjects\\pythonNewProject\\images\\cloud.png"))
-my_img2 = ImageTk.PhotoImage(Image.open("C:\\NetbeansProjectsWin\\PycharmProjects\\pythonNewProject\\images\\code_c.png"))
-my_img3 = ImageTk.PhotoImage(Image.open("C:\\NetbeansProjectsWin\\PycharmProjects\\pythonNewProject\\images\\code_javascript.png"))
-my_img4 = ImageTk.PhotoImage(Image.open("C:\\NetbeansProjectsWin\\PycharmProjects\\pythonNewProject\\images\\file.png"))
-my_img5 = ImageTk.PhotoImage(Image.open("C:\\NetbeansProjectsWin\\PycharmProjects\\pythonNewProject\\images\\folder.png"))
+my_img1 = ImageTk.PhotoImage(Image.open("C:\\NetbeansProjectsWin\\PycharmProjects\\"
+                                        "pythonNewProject\\images\\cloud.png"))
+my_img2 = ImageTk.PhotoImage(Image.open("C:\\NetbeansProjectsWin\\PycharmProjects\\"
+                                        "pythonNewProject\\images\\code_c.png"))
+my_img3 = ImageTk.PhotoImage(Image.open("C:\\NetbeansProjectsWin\\PycharmProjects\\"
+                                        "pythonNewProject\\images\\code_javascript.png"))
+my_img4 = ImageTk.PhotoImage(Image.open("C:\\NetbeansProjectsWin\\PycharmProjects\\"
+                                        "pythonNewProject\\images\\file.png"))
+my_img5 = ImageTk.PhotoImage(Image.open("C:\\NetbeansProjectsWin\\PycharmProjects\\"
+                                        "pythonNewProject\\images\\folder.png"))
 
 image_list = [my_img1, my_img2, my_img3, my_img4, my_img5]
 
@@ -17,6 +22,15 @@ image_list[2]
 
 my_label = Label(image=my_img1)
 my_label.grid(row=0, column=0, columnspan=3)
+
+
+def forward():
+    return
+
+
+def back():
+    return
+
 
 button_back = Button(root, text="<<")
 button_exit = Button(root, text="EXIT PROGRAM", command=root.quit)
