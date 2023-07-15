bf5da0ee4c871a76c0879c611f11a5c333ade6b6
gUlrichTJ <gracianomathey@gmail.com>
2023-07-15 00:25:50 +0000

    Commit 19.1

--- CodeC++/zfraction/main.cpp
+++ CodeC++/zfraction/main.cpp
@@ -8,5 +8,10 @@ using namespace std;
 int main()
 {
     cout << "Hello world!" << endl;
+    // I hope this will work
+    ZFraction fraction1(2, 7), fraction2(3, 8);
+    ZFraction somme = fraction1 + fraction2;
+
+    cout << fraction1.toString() << " + " << fraction2.toString() << " = " << somme.toString() << endl;
     return 0;
 }
