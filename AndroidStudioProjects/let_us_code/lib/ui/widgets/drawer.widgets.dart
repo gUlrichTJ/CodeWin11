b7a7158b201ce4076793fa6c3f1204ab783de75d
gUlrichTJ <gracianomathey@gmail.com>
2023-07-24 18:32:44 +0000

    Commit change the ways

--- AndroidStudioProjects/let_us_code/lib/ui/widgets/drawer.widgets.dart
+++ AndroidStudioProjects/let_us_code/lib/ui/widgets/drawer.widgets.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:let_us_code/config/global.params.dart';
 
 /**Nous créons un stateless widget que nous appelons drawer*/
 class MyDrawer extends StatelessWidget {
@@ -25,43 +26,19 @@ class MyDrawer extends StatelessWidget {
               ),
             ),
           ),
-          ListTile(
-            title: Text("Counter", style: TextStyle(fontSize: 22),),
-            leading: Icon(Icons.home, color: Colors.grey,),
-            trailing: Icon(Icons.arrow_right, color: Colors.black,),
-            onTap: () {
-              Navigator.of(context).pop();
-              Navigator.pushNamed(context, "/counter");
-            },
-          ),
 
-          // Nous séparons les éléments
-          Divider(height: 2, color: Colors.deepOrange,),
+    ...(GlobalParams.menus as List).map((item) {
+            return ListTile(
+              title: Text('${item['title']}', style: TextStyle(fontSize: 22),),
+              leading: Icon(Icons.home, color: Colors.grey,),
+              trailing: Icon(Icons.arrow_right, color: Colors.black,),
+              onTap: () {
+                Navigator.of(context).pop();
+                Navigator.pushNamed(context, "${item['route']}");
+              },
+            );
+          })
 
-          ListTile(
-            title: Text("Meteo", style: TextStyle(fontSize: 22),),
-            leading: Icon(Icons.cloud, color: Colors.black38,),
-            trailing: Icon(Icons.arrow_right, color: Colors.yellow,),
-            onTap: () {
-              // Pour fermer le menu
-              Navigator.of(context).pop();
-              Navigator.pushNamed(context, "/meteo");
-            },
-          ),
-
-          Divider(height: 3, color: Colors.deepOrangeAccent,),
-
-          ListTile(
-            title: Text("Gallery", style: TextStyle(fontSize: 22),),
-            leading: Icon(Icons.browse_gallery, color: Colors.blueGrey,),
-            trailing: Icon(Icons.arrow_right, color: Colors.deepPurple,),
-            onTap: () {
-              Navigator.of(context).pop();  // Pour fermer le menu après clic
-              Navigator.pushNamed(context, "/gallery");
-            },
-          ),
-
-          Divider(height: 2, color: Colors.deepPurple,),
         ],
       ),
     );
