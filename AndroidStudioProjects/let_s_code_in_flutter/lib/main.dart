d6618961be23671372b0ee82ade6f7f1dc8ddf20
gUlrichTJ <gracianomathey@gmail.com>
2023-07-31 16:59:45 +0000

    Add the new github users finder

--- AndroidStudioProjects/let_s_code_in_flutter/lib/main.dart
+++ AndroidStudioProjects/let_s_code_in_flutter/lib/main.dart
@@ -1,23 +1,109 @@
 import 'package:flutter/material.dart';
-import 'package:let_s_code_in_flutter/ui/pages/home.page.dart';
+import './quiz.dart';
+import './camera.dart';
+import './gallery.dart';
+import './weather.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      routes: {
-        "home": (context)=>HomePage()
-      },
-
-      // Le theme
-      theme: ThemeData(
-        primarySwatch: Colors.lightBlue,
+    return Scaffold(
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            DrawerHeader(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Colors.deepOrange,
+                      Colors.white38,
+                    ],
+                  ),
+                ),
+                child: Center(
+                  child: CircleAvatar(
+                    radius: 50,
+                    backgroundImage: AssetImage(''),
+                  ),
+                )),
+            ListTile(
+              title: Text(
+                'Quiz',
+                style: TextStyle(fontSize: 20),
+              ),
+              onTap: () {
+                Navigator.of(context).pop();
+                Navigator.push(
+                    (context), MaterialPageRoute(builder: (context) => Quiz()));
+              },
+            ),
+            Divider(
+              height: 2,
+              color: Colors.deepOrange,
+            ),
+            ListTile(
+              title: Text(
+                'Weather',
+                style: TextStyle(fontSize: 20),
+              ),
+              onTap: () {
+                Navigator.of(context).pop();
+                Navigator.push((context),
+                    MaterialPageRoute(builder: (context) => Weather()));
+              },
+            ),
+            Divider(
+              height: 2,
+              color: Colors.deepOrange,
+            ),
+            ListTile(
+              title: Text(
+                'Gallery',
+                style: TextStyle(fontSize: 20),
+              ),
+              onTap: () {
+                Navigator.of(context).pop();
+                Navigator.push((context),
+                    MaterialPageRoute(builder: (context) => Gallery()));
+              },
+            ),
+            Divider(
+              height: 2,
+              color: Colors.deepOrange,
+            ),
+            ListTile(
+              title: Text(
+                'Camera',
+                style: TextStyle(fontSize: 20),
+              ),
+              onTap: () {
+                Navigator.of(context).pop();
+                Navigator.push((context),
+                    MaterialPageRoute(builder: (context) => CameraPage()));
+              },
+            ),
+          ],
+        ),
       ),
-      home: HomePage(),
+      appBar: AppBar(
+
+        title: Text("My App"),
+        backgroundColor: Colors.deepOrange,
+      ),
+      body: Center(
+        child: Text(
+          'Hello IIBDCC',
+          style: TextStyle(fontSize: 22, color: Colors.deepOrange),
+        ),
+      ),
     );
   }
 }
