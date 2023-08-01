5b7e4e9b6eefd41c1d198b302438313415e2e5ae
gUlrichTJ <gracianomathey@gmail.com>
2023-08-01 20:04:27 +0000

    I'm not finding it

---
+++ IdeaProjects/ideaFp/src/ad/univlome/tg/chat/verticalMenuBar/VerticalMenuBar8.java
@@ -0,0 +1,66 @@
+package ad.univlome.tg.chat.verticalMenuBar;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+
+public class VerticalMenuBar8 extends JFrame {
+
+    // Constructor
+    public VerticalMenuBar8() {
+        super("Vertical Menu Bar");
+        setSize(new Dimension(400, 200));
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        JMenuBar menuBar = new JMenuBar();
+        menuBar.setLayout(new FlowLayout(FlowLayout.LEFT)); // Utiliser un FlowLayout avec alignement à gauche
+
+        // Créer et ajouter les JMenu au JMenuBar
+        addVerticalMenu(menuBar, "File");
+        addVerticalMenu(menuBar, "Edit");
+        addVerticalMenu(menuBar, "View");
+        addVerticalMenu(menuBar, "Help");
+
+        add(menuBar, BorderLayout.WEST);
+
+        setVisible(true);
+    }
+
+    // Méthode pour créer et ajouter un JMenu avec mots penchés (JMenuItem) au JMenuBar
+    private void addVerticalMenu(JMenuBar menuBar, String text) {
+        JMenu menu = new VerticalMenu(text);
+        menuBar.add(menu);
+    }
+
+    // Classe personnalisée pour le JMenu avec mots penchés (JMenuItem)
+    class VerticalMenu extends JMenu {
+
+        public VerticalMenu(String text) {
+            super(text);
+        }
+
+        @Override
+        public Dimension getPreferredSize() {
+            Dimension size = super.getPreferredSize();
+            return new Dimension(size.height, size.width); // Echanger les dimensions pour obtenir un JMenu horizontal
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            Graphics2D g2 = (Graphics2D) g.create();
+            AffineTransform at = new AffineTransform();
+            at.setToRotation(Math.toRadians(90), 0, 0); // Tourner le texte à 90 degrés
+            g2.setTransform(at);
+            super.paintComponent(g2);
+            g2.dispose();
+        }
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new VerticalMenuBar8();
+        });
+    }
+}
