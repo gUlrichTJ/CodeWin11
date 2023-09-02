d62322687f5ffa52987ede65ee041bccc09fa941
gUlrichTJ <gracianomathey@gmail.com>
2023-09-02 23:15:17 +0000

    Learn app_widget

--- AndroidStudioProjects/app_widgets/lib/main.dart
+++ AndroidStudioProjects/app_widgets/lib/main.dart
@@ -1,125 +1,279 @@
+import 'dart:convert';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
+import 'package:app_widget/app_widget.dart';
+
+void onClickWidget(String? payload) {
+  // print('onClick Widget: $payload');
+}
+
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late final AppWidgetPlugin _appWidgetPlugin;
+  late final TextEditingController _controller;
+  int? _widgetId;
+
+  @override
+  void initState() {
+    super.initState();
+    _appWidgetPlugin = AppWidgetPlugin(
+      // androidPackageName: 'tech.noxasch.app_widget_example',
+      onConfigureWidget: onConfigureWidget,
+      onClickWidget: onClickWidget,
+    );
+    _controller = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void onConfigureWidget(int widgetId) {
+    setState(() => _widgetId = widgetId);
+    // do something
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('AppWidgetPlugin example app'),
+        ),
+        body: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(30.0),
+                  child: TextField(
+                    decoration: const InputDecoration(label: Text('Widget Id')),
+                    controller: _controller,
+                    keyboardType: TextInputType.number,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                ConfigureButton(
+                    widgetId: _widgetId, appWidgetPlugin: _appWidgetPlugin),
+                const SizedBox(
+                  height: 10,
+                ),
+                WidgetExistButton(
+                  controller: _controller,
+                  appWidgetPlugin: _appWidgetPlugin,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                ReloadWidgetButton(appWidgetPlugin: _appWidgetPlugin),
+                const SizedBox(
+                  height: 10,
+                ),
+                UpdateWidgetButton(
+                    controller: _controller, appWidgetPlugin: _appWidgetPlugin),
+                const SizedBox(
+                  height: 10,
+                ),
+                GetWidgetIdsButton(appWidgetPlugin: _appWidgetPlugin),
+              ],
+            ),
+          ),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class UpdateWidgetButton extends StatelessWidget {
+  const UpdateWidgetButton({
+    Key? key,
+    required TextEditingController controller,
+    required AppWidgetPlugin appWidgetPlugin,
+  })  : _controller = controller,
+        _appWidgetPlugin = appWidgetPlugin,
+        super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final TextEditingController _controller;
+  final AppWidgetPlugin _appWidgetPlugin;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    return ElevatedButton(
+      onPressed: () async {
+        if (_controller.text.isNotEmpty) {
+          // this means the app is started by the widget config event
+          final widgetId = int.parse(_controller.text);
+
+          // send configure
+          await _appWidgetPlugin.updateWidget(
+            widgetId: widgetId,
+            widgetLayout: 'example_layout',
+            textViews: {
+              'widget_title': 'App Widget',
+              'widget_message': 'Updated in flutter'
+            },
+            payload: jsonEncode({'number': Random.secure().nextInt(10)}),
+          );
+        }
+      },
+      child: const Text('Update Widget'),
     );
+  }
+}
+
+class GetWidgetIdsButton extends StatelessWidget {
+  const GetWidgetIdsButton({
+    Key? key,
+    required AppWidgetPlugin appWidgetPlugin,
+  })  : _appWidgetPlugin = appWidgetPlugin,
+        super(key: key);
+
+  final AppWidgetPlugin _appWidgetPlugin;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () async {
+        final messenger = ScaffoldMessenger.of(context);
+        final widgetIds = await _appWidgetPlugin.getWidgetIds(
+            androidProviderName: 'AppWidgetExampleProvider');
+        messenger.showSnackBar(
+          SnackBar(
+            content: Text('widget ids: ${widgetIds ?? ""}'),
+          ),
+        );
+      },
+      child: const Text('Get WidgetIds'),
+    );
+  }
+}
+
+class ReloadWidgetButton extends StatelessWidget {
+  const ReloadWidgetButton({
+    Key? key,
+    required AppWidgetPlugin appWidgetPlugin,
+  })  : _appWidgetPlugin = appWidgetPlugin,
+        super(key: key);
+
+  final AppWidgetPlugin _appWidgetPlugin;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () async {
+        final messenger = ScaffoldMessenger.of(context);
+        await _appWidgetPlugin.reloadWidgets(
+          androidProviderName: 'AppWidgetExampleProvider',
+        );
+        messenger.showSnackBar(
+          const SnackBar(
+            content:
+            Text('Reload broadcast has been sent check Android debug log'),
+          ),
+        );
+      },
+      child: const Text('Reload Widgets'),
+    );
+  }
+}
+
+class WidgetExistButton extends StatelessWidget {
+  const WidgetExistButton({
+    Key? key,
+    required TextEditingController controller,
+    required AppWidgetPlugin appWidgetPlugin,
+  })  : _controller = controller,
+        _appWidgetPlugin = appWidgetPlugin,
+        super(key: key);
+
+  final TextEditingController _controller;
+  final AppWidgetPlugin _appWidgetPlugin;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        onPressed: () async {
+          if (_controller.text.isNotEmpty) {
+            final messenger = ScaffoldMessenger.of(context);
+
+            final widgetId = int.parse(_controller.text);
+            final exist = (await _appWidgetPlugin.widgetExist(widgetId))!;
+            if (exist) {
+              messenger.showSnackBar(
+                const SnackBar(
+                  content: Text('This widget exist.'),
+                ),
+              );
+            } else {
+              messenger.showSnackBar(
+                SnackBar(
+                  content: Text('Widget with id $widgetId does not exist.'),
+                ),
+              );
+            }
+          }
+        },
+        child: const Text('check if Widget Exist'));
+  }
+}
+
+class ConfigureButton extends StatelessWidget {
+  const ConfigureButton({
+    Key? key,
+    required int? widgetId,
+    required AppWidgetPlugin appWidgetPlugin,
+  })  : _widgetId = widgetId,
+        _appWidgetPlugin = appWidgetPlugin,
+        super(key: key);
+
+  final int? _widgetId;
+  final AppWidgetPlugin _appWidgetPlugin;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        onPressed: () async {
+          if (_widgetId != null) {
+            // this means the app is started by the widget config event
+            final messenger = ScaffoldMessenger.of(context);
+
+            // send configure
+            await _appWidgetPlugin.configureWidget(
+              widgetId: _widgetId!,
+              widgetLayout: 'example_layout',
+              textViews: {
+                'widget_title': 'App Widget',
+                'widget_message': 'Configured in flutter'
+              },
+              payload: jsonEncode({'number': Random().nextInt(10)}),
+            );
+            messenger.showSnackBar(
+                const SnackBar(content: Text('Widget has been configured!')));
+          } else {
+            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                content:
+                Text('Opps, no widget id from WIDGET_CONFIGURE event')));
+          }
+        },
+        child: const Text('Configure Widget'));
   }
 }
