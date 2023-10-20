eecc70572adf831dfdf6ed9859bb25248f5b2a9b
gUlrichTJ <gracianomathey@gmail.com>
2023-10-20 16:24:46 +0000

    Winner from scratch on October flutter

--- PycharmProjects/pythonNewProject/chrome-os-codes/automate_boring/dictionaries.py
+++ PycharmProjects/pythonNewProject/chrome-os-codes/automate_boring/dictionaries.py
@@ -1,28 +1,56 @@
 # Dictionaries
 import pprint
 
+# We create a function to take the number of lines or columns
+
+
+def dimension(label):
+    while True:
+        lines = input(f"Number of {str(label)} : ")
+        if not lines.isdigit():
+            print("")
+
+
+def string_store(table, n, c):  # We're going to store string in a table
+    print()
+    while True:
+        lines = input("Lines : ")
+        while not lines.isdigit():  # We verify the number of lines entered is a digit
+            print("The number of lines must be a digit.\nLines : ", end="")
+            lines = input()
+        columns = input("Columns : ")
+        while not lines.isdigit():
+            print("The number of columns must be a digit.\nColumns : ", end="")
+            columns = input()
+
 
 # We're going to enter datas in a table of two dimensions
 
-def two_dim_table(tab, l, c):
+def two_dim_table(table, l, c):
     # We enter datas
     for i in range(l):
         for j in range(c):
             print(f"Enter value [{i+1}][{j+1}] : ")
             data = input()
-            if data.isdigit():
-                tab[i][j] = data
-            else:
-                print("You must enter an integer")
+            while not data.isdigit():
+                print("You must enter an integer ", end=" ")
+                data = input()
 
+            table[i][j] = data
+
+    print()
     for i in range(l):
         for j in range(c):
-            print(f"[{i}][{j}]", end=" ")
+            print(f"{table[i][j]:>15}", end=" ")
         print()
+        if i < l - 1:
+            print("|", end="")
+
 
 # We create a table were we put the integers
-
-
+tab = [[0 for i in range(10)] for j in range(10)]
+two_dim_table(tab, l=5, c=4)
+print()
 # strip and ...
 message = "Éloignez o Seigneur Jesus, les attractions de ce monde   ."
 message.strip()
