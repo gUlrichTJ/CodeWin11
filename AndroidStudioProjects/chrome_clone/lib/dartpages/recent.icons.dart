7e5eb4c7803d26477a5a7f5fb4af33d43e75e599
gUlrichTJ <gracianomathey@gmail.com>
2023-11-03 18:42:55 +0000

    Commit changes and learn table

--- AndroidStudioProjects/chrome_clone/lib/dartpages/recent.icons.dart
+++ AndroidStudioProjects/chrome_clone/lib/dartpages/recent.icons.dart
@@ -1,2 +1,15 @@
 import 'package:flutter/material.dart';
 
+class RecentIcons extends StatelessWidget {
+  const RecentIcons({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      /// The width of the container
+      width: MediaQuery.of(context).size.width * 0.8,
+      child: Table(
+      ),
+    );
+  }
+}
--- AndroidStudioProjects/test_of_codes/lib/main.dart
+++ AndroidStudioProjects/test_of_codes/lib/main.dart
@@ -1,78 +1,81 @@
-// TODO: Comment utiliser sizedBox et Stack. Stack permet d'avoir une pile
 import 'package:flutter/material.dart';
 
-/// Flutter code sample for [SearchBar].
+/// Flutter code sample for [Table].
 
-void main() => runApp(const SearchBarApp());
+void main() => runApp(const TableExampleApp());
 
-class SearchBarApp extends StatefulWidget {
-  const SearchBarApp({super.key});
-
-  @override
-  State<SearchBarApp> createState() => _SearchBarAppState();
-}
-
-class _SearchBarAppState extends State<SearchBarApp> {
-  bool isDark = false;
+class TableExampleApp extends StatelessWidget {
+  const TableExampleApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final ThemeData themeData = ThemeData(
-        useMaterial3: true,
-        brightness: isDark ? Brightness.dark : Brightness.light);
-
     return MaterialApp(
-      theme: themeData,
       home: Scaffold(
-        appBar: AppBar(title: const Text('Search Bar Sample')),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: SearchAnchor(
-              builder: (BuildContext context, SearchController controller) {
-                return SearchBar(
-                  controller: controller,
-                  padding: const MaterialStatePropertyAll<EdgeInsets>(
-                      EdgeInsets.symmetric(horizontal: 16.0)),
-                  onTap: () {
-                    controller.openView();
-                  },
-                  onChanged: (_) {
-                    controller.openView();
-                  },
-                  leading: const Icon(Icons.search),
-                  trailing: <Widget>[
-                    Tooltip(
-                      message: 'Change brightness mode',
-                      child: IconButton(
-                        isSelected: isDark,
-                        onPressed: () {
-                          setState(() {
-                            isDark = !isDark;
-                          });
-                        },
-                        icon: const Icon(Icons.wb_sunny_outlined),
-                        selectedIcon: const Icon(Icons.brightness_2_outlined),
-                      ),
-                    )
-                  ],
-                );
-              }, suggestionsBuilder:
-              (BuildContext context, SearchController controller) {
-            return List<ListTile>.generate(5, (int index) {
-              final String item = 'item $index';
-              return ListTile(
-                title: Text(item),
-                // Stay focused ok ?
-                onTap: () {
-                  setState(() {
-                    controller.closeView(item);
-                  });
-                },
-              );
-            });
-          }),
-        ),
+        appBar: AppBar(title: const Text('Table Sample')),
+        body: const TableExample(),
       ),
+    );
+  }
+}
+
+class TableExample extends StatelessWidget {
+  const TableExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Table(
+      border: TableBorder.all(),
+      columnWidths: const <int, TableColumnWidth>{
+        0: IntrinsicColumnWidth(),
+        1: FlexColumnWidth(),
+        2: FixedColumnWidth(64),
+      },
+      defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+      children: <TableRow>[
+        TableRow(
+          children: <Widget>[
+            Container(
+              height: 32,
+              color: Colors.green,
+            ),
+            TableCell(
+              verticalAlignment: TableCellVerticalAlignment.top,
+              child: Container(
+                height: 32,
+                width: 32,
+                color: Colors.red,
+              ),
+            ),
+            Container(
+              height: 64,
+              color: Colors.blue,
+            ),
+          ],
+        ),
+        TableRow(
+          decoration: const BoxDecoration(
+            color: Colors.grey,
+          ),
+          children: <Widget>[
+            Container(
+              height: 64,
+              width: 128,
+              color: Colors.purple,
+            ),
+            Container(
+              height: 32,
+              color: Colors.yellow,
+            ),
+            Center(
+              child: Container(
+                height: 32,
+                width: 32,
+                color: Colors.orange,
+              ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
