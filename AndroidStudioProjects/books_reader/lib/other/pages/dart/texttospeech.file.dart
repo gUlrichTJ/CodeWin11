d87d2cf53649462d7ff928cefcfe7453507a5154
gUlrichTJ <gracianomathey@gmail.com>
2023-11-04 12:50:18 +0000

    Commit changes and thank God

--- AndroidStudioProjects/books_reader/lib/other/pages/dart/texttospeech.file.dart
+++ AndroidStudioProjects/books_reader/lib/other/pages/dart/texttospeech.file.dart
@@ -1,9 +1,15 @@
 import 'package:flutter_tts/flutter_tts.dart';
 
+import 'package:flutter/services.dart';
+
 class TextToSpeechManager {
   /// We create an instance of FlutterTts
   FlutterTts flutterTts = FlutterTts();
-
+  TextToSpeechManager() {
+    flutterTts.setLanguage("en-US");
+    flutterTts.setSpeechRate(0.70);
+    flutterTts.setPitch(1.0);
+  }
   /// Méthode pour lire le texte à haute voix
   Future<void> speak(String text) async {
     await flutterTts.speak(text);
