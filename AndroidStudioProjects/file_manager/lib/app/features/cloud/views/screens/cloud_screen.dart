c8b74afba03c93c70e4277468cbb38eb1738c561
gUlrichTJ <gracianomathey@gmail.com>
2023-11-13 22:07:32 +0000

    Commit changes

--- AndroidStudioProjects/file_manager/lib/app/features/cloud/views/screens/cloud_screen.dart
+++ AndroidStudioProjects/file_manager/lib/app/features/cloud/views/screens/cloud_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class CloudSreen extends StatelessWidget {
   const CloudSreen({Key? key}) : super(key: key);
 
+  /// God's Great everytime
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
