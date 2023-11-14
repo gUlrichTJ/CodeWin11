16f78d17822bfcc35d8a28d12d719d00725185bf
gUlrichTJ <gracianomathey@gmail.com>
2023-11-14 22:37:31 +0000

    Commit changes

--- AndroidStudioProjects/test_of_codes/lib/main.dart
+++ AndroidStudioProjects/test_of_codes/lib/main.dart
@@ -1,29 +1,3 @@
-/*import 'package:flutter/material.dart';
-import 'package:test_of_codes/other_pages/other.page.dart';
-
-void main() => runApp(const MyApp());
-
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Access to folder",
-      home: Pdflist(),
-    );
-  }
-}
-
-
- */
-
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
