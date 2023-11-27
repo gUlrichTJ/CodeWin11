edccf81d5f9f2f25ea32a7051c633e026de35b88
gUlrichTJ <gracianomathey@gmail.com>
2023-11-27 18:18:01 +0000

    Auto commit at

--- IdeaProjects/emploiDuTemps/src/main/java/panels/Authentification.java
+++ IdeaProjects/emploiDuTemps/src/main/java/panels/Authentification.java
@@ -8,10 +8,11 @@ public class Authentification extends JFrame {
    JPanel panel = new JPanel();
    // Constructor
    public Authentification() {
-      this.setTitle("Authentification");
-      this.setSize(500, 500);
+      super("Authentification");
+      this.setSize(700, 500);
       this.setLocationRelativeTo(null);
       this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+      this.setResizable(false);
 
       // Visibility of the window
       this.setVisible(true);
