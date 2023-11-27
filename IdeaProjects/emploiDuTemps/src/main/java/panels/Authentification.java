663037fb2b1d8f6b4386f15302cfc4ff35b5fb5e
gUlrichTJ <gracianomathey@gmail.com>
2023-11-27 18:14:01 +0000

    Auto commit at

--- IdeaProjects/emploiDuTemps/src/main/java/panels/Authentification.java
+++ IdeaProjects/emploiDuTemps/src/main/java/panels/Authentification.java
@@ -2,5 +2,12 @@ package panels;
 
 import javax.swing.*;
 
-public class Authentification extends JPanel {
+public class Authentification extends JFrame {
+
+   // We create a panel
+   JPanel panel = new JPanel();
+   // Constructor
+   public Authentification() {
+      panel.setTitle("Hello");
+   }
 }
