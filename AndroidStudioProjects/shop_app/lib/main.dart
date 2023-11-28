d1536c2d029c8d97a1edee60b66cc9d26f80b3b5
gUlrichTJ <gracianomathey@gmail.com>
2023-11-28 16:06:02 +0000

    Auto commit at

--- AndroidStudioProjects/shop_app/lib/main.dart
+++ AndroidStudioProjects/shop_app/lib/main.dart
@@ -1,3 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/pages/home.page.dart';
 
-void main() => runApp(MyApp)
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
