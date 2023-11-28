bf78aefbc44bb303ea31c59a93c1e28995523ed2
gUlrichTJ <gracianomathey@gmail.com>
2023-11-28 17:22:01 +0000

    Auto commit at

--- AndroidStudioProjects/shop_app/lib/main.dart
+++ AndroidStudioProjects/shop_app/lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: const MyHomePage(),
       theme: ThemeData(
+      primarySwatch: ,
         appBarTheme: const AppBarTheme(
           centerTitle: true,
         )
