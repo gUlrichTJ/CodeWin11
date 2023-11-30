220baa7694eb77e99063e4af0825c84f4830f8b0
gUlrichTJ <gracianomathey@gmail.com>
2023-11-30 09:56:01 +0000

    Auto commit at

--- PycharmProjects/pythonNewProject/front_countries/france_map_turtle_code.py
+++ PycharmProjects/pythonNewProject/front_countries/france_map_turtle_code.py
@@ -1,5 +1,11 @@
 import turtle as france
 
+# We move the pen
 france.penup()
-france.goto(0, 100)
+france.goto(20, 100)
 france.pendown()
+
+france.left(200)
+france.forward(200)
+
+france.mainloop()
