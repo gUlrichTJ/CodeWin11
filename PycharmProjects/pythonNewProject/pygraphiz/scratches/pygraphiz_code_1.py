05746115b10e6365666c82d7d63aeb15310d2960
gUlrichTJ <gracianomathey@gmail.com>
2023-11-30 18:54:01 +0000

    Auto commit at

--- PycharmProjects/pythonNewProject/pygraphiz/scratches/pygraphiz_code_1.py
+++ PycharmProjects/pythonNewProject/pygraphiz/scratches/pygraphiz_code_1.py
@@ -5,7 +5,19 @@ G = pygraphviz.AGraph()
 # We add a node
 G.add_node("A", color="red")
 G.add_edge("A", "B")
+G.add_edge("A", "H")
+G.add_edge("H", "I")
+G.add_edge("I", "J")
+G.add_edge("J", "H")
+G.add_edge("B", "C")
+G.add_edge("C", "D")
+G.add_edge("C", "E")
+G.add_edge("E", "G")
+G.add_edge("G", "F")
+G.add_edge("F", "E")
 G.add_edge("b", "c")
+
+
 
 # We can add node from a nodelist
 node_list = ["f", "g", "h"]
