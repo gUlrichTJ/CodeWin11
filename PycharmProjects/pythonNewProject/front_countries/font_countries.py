b880f743d67ffab04b3822ff416f8a11fc17d5ab
gUlrichTJ <gracianomathey@gmail.com>
2023-11-30 04:36:01 +0000

    Auto commit at

--- PycharmProjects/pythonNewProject/front_countries/font_countries.py
+++ PycharmProjects/pythonNewProject/front_countries/font_countries.py
@@ -1,13 +1,13 @@
-import turtle
+import turtle as village
 
-turtle.speed(2)
-turtle.forward(200)
-turtle.left(120)
-turtle.forward(200)
-turtle.left(120)
-turtle.forward(200)
+village.speed(2)
+village.begin_fill()
+village.forward(200)
+village.left(120)
+village.forward(200)
+village.left(120)
+village.forward(200)
 
 # TODO: We fill the first village
-turtle.fillcolor("red")
-turtle.begin_fill()
-turtle.end_fill()
+village.fillcolor("red")
+village.end_fill()
