74a5d18deb895c2836513147664ecb1e832bf686
gUlrichTJ <gracianomathey@gmail.com>
2023-11-30 04:42:01 +0000

    Auto commit at

--- PycharmProjects/pythonNewProject/front_countries/font_countries.py
+++ PycharmProjects/pythonNewProject/front_countries/font_countries.py
@@ -3,13 +3,16 @@ import turtle as village
 # TODO : The title of our window
 village.title("We draw villages")
 
+# TODO: The size of villages
+village_size = 100
+
 village.speed(2)
 village.begin_fill()
-village.forward(200)
+village.forward(village_size)
 village.left(120)
-village.forward(200)
+village.forward(village_size)
 village.left(120)
-village.forward(200)
+village.forward(village_size)
 
 # TODO: We fill the first village
 village.fillcolor("red")
@@ -17,6 +20,6 @@ village.end_fill()
 
 # TODO: We turn to draw the second village
 village.left(30)
-village.forward(200)
+village.forward(village_size)
 
 village.mainloop()
