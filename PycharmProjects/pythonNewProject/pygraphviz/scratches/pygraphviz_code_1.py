648f051afc21ffa5f556d3c9426b8a11826a6eab
gUlrichTJ <gracianomathey@gmail.com>
2023-11-30 19:22:02 +0000

    Auto commit at

--- PycharmProjects/pythonNewProject/pygraphviz/scratches/pygraphviz_code_1.py
+++ PycharmProjects/pythonNewProject/pygraphviz/scratches/pygraphviz_code_1.py
@@ -3,22 +3,23 @@ import pygraphviz
 G = pygraphviz.AGraph()
 
 # We add a node
-# G.add_node("A", color="red")
+G.add_node("A", color="red")
 G.add_node("B", color="blue")
-# G.add_node("C", color="red")
+G.add_node("C", color="red")
 G.add_node("D", color="blue")
 G.add_node("E", color="blue")
-# G.add_node("F", color="red")
+G.add_node("F", color="red")
 G.add_node("G", color="darkgreen")
 G.add_node("H", color="blue")
 G.add_node("I", color="darkgreen")
-# G.add_node("J", color="red")
+G.add_node("J", color="red")
 
+"""
 # Node colors
 nodes_labels = ["A", "C", "F", "J",]
 # We are going to set them to red color
-G.add_nodes_from((G, {"color": "red"}) for node in nodes_labels)
-
+G.add_nodes_from((node, {"color": "red"}) for node in nodes_labels)
+"""
 G.add_edge("A", "B")
 G.add_edge("A", "H")
 G.add_edge("H", "I", color="blue")
@@ -39,4 +40,4 @@ node_list = ["f", "g", "h"]
 G.add_node(node_list)
 
 G.layout()
-G.draw("pygraphvis.png")
+G.draw("images/pygraphvis.png")
