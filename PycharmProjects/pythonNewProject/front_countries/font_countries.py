4bb296dd2e34502eeea60c2c7013f40763910e76
gUlrichTJ <gracianomathey@gmail.com>
2023-11-30 04:50:01 +0000

    Auto commit at

--- PycharmProjects/pythonNewProject/front_countries/font_countries.py
+++ PycharmProjects/pythonNewProject/front_countries/font_countries.py
@@ -1,3 +1,4 @@
+import turtle
 import turtle as village
 
 # TODO : The title of our window
@@ -39,5 +40,36 @@ village.right(90)
 village.forward(village_size / 2)
 village.fillcolor("pink")
 village.end_fill()
+
+# We move to the end of the second village
+turtle.forward(village_size / 2)
+
+
+
+# TODO: We turn to draw the second village
+village.begin_fill()
+village.left(30)
+village.forward(village_size)
+village.left(90)
+village.forward(village_size / 2)
+village.left(90)
+village.forward(village_size)
+village.fillcolor("grey")
+village.end_fill()
+
+# TODO: We draw the third village
+village.begin_fill()
+village.right(90)
+village.forward(village_size / 2)
+village.right(90)
+village.forward(village_size)
+village.right(90)
+village.forward(village_size / 2)
+village.fillcolor("pink")
+village.end_fill()
+
+# We move to the end of the second village
+turtle.forward(village_size / 2)
+
 
 village.mainloop()
