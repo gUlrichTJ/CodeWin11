5f615792d7f3e02dc761ffde4c3226cb83d33800
gUlrichTJ <gracianomathey@gmail.com>
2023-11-30 08:36:01 +0000

    Auto commit at

--- PycharmProjects/pythonNewProject/front_countries/font_countries.py
+++ PycharmProjects/pythonNewProject/front_countries/font_countries.py
@@ -52,8 +52,8 @@ village.forward(village_size / 2)
 
 # TODO: We draw the rest of the villages
 # TODO: We put the colors in a table
-colors = ["red", "yellow", "indigo"]
-colours = ["purple", "red", "yellow"]
+colors = ["red", "indigo", "red"]
+colours = ["yellow", "purple", "yellow"]
 
 for i in range(3):
     # TODO: We turn to draw the second village
