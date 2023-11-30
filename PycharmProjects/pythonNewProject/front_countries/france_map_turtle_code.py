0e00eda76f32513a4068c34b7e7d910f1ba57307
gUlrichTJ <gracianomathey@gmail.com>
2023-11-30 13:50:02 +0000

    Auto commit at

--- PycharmProjects/pythonNewProject/front_countries/france_map_turtle_code.py
+++ PycharmProjects/pythonNewProject/front_countries/france_map_turtle_code.py
@@ -15,12 +15,13 @@ while i < 10:
     pass
 
 france.penup()
-france.forward(300)
+france.forward(30)
 france.pendown()
 
-for i in range(0, 100, 2):
-    france.forward(i)
+for i in range(0, 200, 2):
+    france.forward(i - i / 2)
     france.left(i)
+    france.forward(i)
 
 
 #france.left(200)
