36d465e776fa80119324cce8d20798ea7b0237fc
gUlrichTJ <gracianomathey@gmail.com>
2023-11-30 19:42:02 +0000

    Auto commit at

--- PycharmProjects/pythonNewProject/pygraphviz/codes/code_30_nov_2023/pygraphviz2.py
+++ PycharmProjects/pythonNewProject/pygraphviz/codes/code_30_nov_2023/pygraphviz2.py
@@ -3,4 +3,8 @@ import pygraphviz as pgv
 G = pgv.AGraph()
 
 # We create the nodes
-G.add_node("1")
+G.add_node("1", color="red")
+G.add_node("5", color="blue")
+G.add_edge("1", "2", color="green")
+G.add_edge("1", "3")
+G.add_edge("2", "3")
