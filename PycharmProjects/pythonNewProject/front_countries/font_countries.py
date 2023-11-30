7cf78d116b2b93245cc8d1d95e92883bb7a1dc15
gUlrichTJ <gracianomathey@gmail.com>
2023-11-30 05:18:01 +0000

    Auto commit at

--- PycharmProjects/pythonNewProject/front_countries/font_countries.py
+++ PycharmProjects/pythonNewProject/front_countries/font_countries.py
@@ -83,10 +83,10 @@ village.forward(village_size)
 village.right(10)
 village.forward(village_size / 2)
 
-village.right(20)
+village.right(10)
 village.forward(village_size)
 
-village.left(160)
+village.left(150)
 village.forward(village_size)
 
 village.mainloop()
