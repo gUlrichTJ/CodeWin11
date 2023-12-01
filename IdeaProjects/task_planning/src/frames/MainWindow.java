f5687310c62857a190539e0174098637463765de
gUlrichTJ <gracianomathey@gmail.com>
2023-12-01 16:00:01 +0000

    Auto commit at

--- IdeaProjects/task_planning/src/frames/MainWindow.java
+++ IdeaProjects/task_planning/src/frames/MainWindow.java
@@ -1,2 +1,16 @@
-package frames;public class MainWindow {
+package frames;
+
+import javax.swing.*;
+
+public class MainWindow extends JFrame {
+
+   // Constructor
+   public MainWindow() {
+      super("Authentification");
+      setSize(400, 400);
+      setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+      setLocationRelativeTo(null);
+
+      setVisible(true);
+   }
 }
