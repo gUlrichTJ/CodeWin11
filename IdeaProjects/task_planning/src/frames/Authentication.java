00f79776dc2f71e723f9f1716b002b593295c905
gUlrichTJ <gracianomathey@gmail.com>
2023-12-01 16:14:01 +0000

    Auto commit at

--- IdeaProjects/task_planning/src/frames/Authentication.java
+++ IdeaProjects/task_planning/src/frames/Authentication.java
@@ -1,9 +1,11 @@
 package frames;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Authentication extends JFrame {
 
+   JPanel mainPanel = new JPanel();
    // Constructor
    public Authentication() {
       super("Authentication");
@@ -11,6 +13,27 @@ public class Authentication extends JFrame {
       setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
       setLocationRelativeTo(null);
 
+      mainPanel.add(login(), BorderLayout.CENTER);
+
+      add(mainPanel);
+
       setVisible(true);
+   }
+
+   /// Method to send the login
+   JPanel login() {
+      JPanel panel = new JPanel();
+      panel.setLayout(new GridLayout(1, 2));
+
+      /// TODO: the username
+      JLabel usernameLabel = new JLabel("Username");
+      // We add the label username to the panel
+      panel.add(usernameLabel);
+
+      JTextField usernameField = new JTextField();
+
+      // We add the textField to the panel
+      panel.add(usernameField);
+      return panel;
    }
 }
