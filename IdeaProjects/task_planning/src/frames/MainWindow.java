afa350ed5c85d37258bb1fce9c7374a80f329b9b
gUlrichTJ <gracianomathey@gmail.com>
2023-12-01 16:02:02 +0000

    Auto commit at

--- IdeaProjects/task_planning/src/frames/MainWindow.java
+++ IdeaProjects/task_planning/src/frames/MainWindow.java
@@ -6,8 +6,8 @@ public class MainWindow extends JFrame {
 
    // Constructor
    public MainWindow() {
-      super("Authentification");
-      setSize(400, 400);
+      super("Authentication");
+      setSize(600, 400);
       setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
       setLocationRelativeTo(null);
 
