ccf05f6e813ae56d9e999f069a5bb6195d2b487e
gUlrichTJ <gracianomathey@gmail.com>
2023-12-06 19:20:02 +0000

    Auto commit at

--- AndroidStudioProjects/test_of_codes/lib/main.dart
+++ AndroidStudioProjects/test_of_codes/lib/main.dart
@@ -33,6 +33,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(),
       body: Column(
         children: <Widget>[
           IconButton(onPressed: _onPressed,
