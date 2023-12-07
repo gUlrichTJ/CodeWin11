dc5df9308698a3542a1ad0ee3dbaa593328466f7
gUlrichTJ <gracianomathey@gmail.com>
2023-12-07 12:42:01 +0000

    Auto commit at

--- AndroidStudioProjects/test_of_codes/lib/main.dart
+++ AndroidStudioProjects/test_of_codes/lib/main.dart
@@ -1,6 +1,5 @@
-/*
 import 'package:flutter/material.dart';
-
+/*
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
@@ -117,4 +116,41 @@ class _HomePageState extends State<HomePage> {
 }
 
  */
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.lightGreen,
+        canvasColor: Colors.lightGreen.shade200,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          
+        ),
+      ),
+    );
+  }
+}
 
