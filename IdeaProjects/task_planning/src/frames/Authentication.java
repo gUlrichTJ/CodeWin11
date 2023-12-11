50144cccbbae12eb00d52330e67d8e0b23e586d1
gUlrichTJ <gracianomathey@gmail.com>
2023-12-11 18:48:02 +0000

    Auto commit at

--- IdeaProjects/task_planning/src/frames/Authentication.java
+++ IdeaProjects/task_planning/src/frames/Authentication.java
@@ -95,7 +95,7 @@ public class Authentication extends JFrame {
       passwordAndField.add(passwordLabel);
 
       passwordField = new JPasswordField();
-      passwordAndField.setBorder(new BevelBorder(10, Color.black, Color.BLUE));
+      
       passwordAndField.setPreferredSize(new Dimension((int) (getWidth() * 0.7), 30));
 
       passwordAndField.add(passwordField);
