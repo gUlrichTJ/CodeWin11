c511ec882a4322f017ef3b4b7f9133457ca7f3d6
gUlrichTJ <gracianomathey@gmail.com>
2023-12-12 01:16:01 +0000

    Auto commit at

--- IdeaProjects/task_planning/src/frames/Authentication.java
+++ IdeaProjects/task_planning/src/frames/Authentication.java
@@ -142,25 +142,9 @@ public class Authentication extends JFrame implements JTotal {
 
       // TODO: The code of stackoverflow
       AbstractBorder borderLeft = new TextBubbleBorder(
-            Color.black, 2, 10, 6
+            Color.white, 2, 20, 1
       );
       cancel.setBorder(borderLeft);
-      Border roundedBorder = new Border() {
-         @Override
-         public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
-
-         }
-
-         @Override
-         public Insets getBorderInsets(Component c) {
-            return null;
-         }
-
-         @Override
-         public boolean isBorderOpaque() {
-            return false;
-         }
-      };
 
       // TODO: Action to the login button
       login.addActionListener(e -> {
