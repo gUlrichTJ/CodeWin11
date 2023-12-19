1d91c2ef2175e45a8f251b341c90c087dda3efbf
gUlrichTJ <gracianomathey@gmail.com>
2023-12-19 17:18:01 +0000

    Auto commit at

--- CodeC++/mathematics_calculus/main.cpp
+++ CodeC++/mathematics_calculus/main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 #include <cmath>
+#include <ctime>
 
 using namespace std;
 
 /// We create the function quickly
     void return_n_numbers(int n, int max_terms) {
+
+        /// We will know the time the program took to be executed
+        /// We start the clock at the begining of the program
+        clock_t start_program = clock();
+
         int total_number = 0, x = 0, y = 0, z = 0;
 
         /// loop
@@ -21,6 +27,9 @@ using namespace std;
         }
         /// We print the total numbers
         cout << total_number << endl;
+
+        /// We end the clock at the end of the program
+        clock_t end_program = clock();
     }
 
 int main()
