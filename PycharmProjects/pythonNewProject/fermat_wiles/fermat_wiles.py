970e0d9d89ea9d6af8ed6699c253877e23bcd1f2
gUlrichTJ <gracianomathey@gmail.com>
2023-12-19 18:00:01 +0000

    Auto commit at

--- PycharmProjects/pythonNewProject/fermat_wiles/fermat_wiles.py
+++ PycharmProjects/pythonNewProject/fermat_wiles/fermat_wiles.py
@@ -7,9 +7,9 @@ def returnNumbers(n, max_n):
             for z in range(1, max_n):
                 if x**n + y**n == z**n:
                     i += 1
-                    # print(x, y, z)
+                    print(x, y, z)
     print(i)
 
 
 # Now, we try it with n = 100
-returnNumbers(2, 1000)
+returnNumbers(2, 100)
