7c5132ce0f137ea5bef419ebc7bb043559b0bba3
gUlrichTJ <gracianomathey@gmail.com>
2023-12-19 18:08:01 +0000

    Auto commit at

--- PycharmProjects/pythonNewProject/fermat_wiles/fermat_wiles.py
+++ PycharmProjects/pythonNewProject/fermat_wiles/fermat_wiles.py
@@ -1,14 +1,16 @@
 # We are going to try to show out the integers
 # smaller than n that verify the equation x^2 + y^2 = z^2
 def returnNumbers(n, max_n):
-    i = 0
+    temp = [0]
     for x in range(1, max_n):
         for y in range(1, max_n):
+            if y in temp:
+                break
             for z in range(1, max_n):
                 if x**n + y**n == z**n:
-                    i += 1
+                    temp.append(x)
                     print(x, y, z)
-    print(i)
+    print(len(temp))
 
 
 # Now, we try it with n = 100
