112eddbdfa05de28393b2c3683c935056eeaee86
gUlrichTJ <gracianomathey@gmail.com>
2023-12-21 13:06:01 +0000

    Auto commit at

--- AndroidStudioProjects/test_of_codes/lib/main.dart
+++ AndroidStudioProjects/test_of_codes/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 // import 'package:permission_handler/permission_handler.dart';
 /*
 void main() => runApp(const MyApp());
@@ -146,10 +147,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  // List of todomenuitem
-  List<TodoMenuItem> foodMenuItem [
-    TodoMenuItem()
-  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
