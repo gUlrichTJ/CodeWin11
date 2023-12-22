a606d003288194dd74f475a0b98fae3fb77b4ff7
gUlrichTJ <gracianomathey@gmail.com>
2023-12-22 10:52:02 +0000

    Auto commit at

--- AndroidStudioProjects/code_book_reading/lib/main.dart
+++ AndroidStudioProjects/code_book_reading/lib/main.dart
@@ -51,7 +51,7 @@ class HomePage extends StatelessWidget {
                       ],
                     ),
                 );
-              }) ;
+              }).toList() ;
             },
           ),
         ],
