081c4e7ce9a49a98f48fb159989daacb5ed64220
gUlrichTJ <gracianomathey@gmail.com>
2023-12-30 13:40:01 +0000

    Auto commit at

--- CodeC/finish_the_book_of_c/c_tables/main.c
+++ CodeC/finish_the_book_of_c/c_tables/main.c
@@ -3,7 +3,23 @@
 
 /// Function that calculates the sum, product and average of numbers
 void sum_product_average(int N) {
+    if (N < 2 || N > 100) {
+        printf("The number you put in the function is not correct !");
+    }
 
+    /// Let's calculate the product
+    for (int i = 1; i < N + 1; i++) {
+        sum += i;
+        product *= i;
+    }
+
+    printf("Sum : %lf\n", sum);
+    printf("Product : %lf\n", product);
+    printf("Average : %lf\n", sum / N);
+
+    do {
+        printf()
+    } while(N < 2 || N > 100);
 }
 int main()
 {
