dabd09ec53eaf42e458da3ceeb0d5fbb7d4b572b
gUlrichTJ <gracianomathey@gmail.com>
2023-12-31 22:32:01 +0000

    Auto commit at

--- CodeC/finish_the_book_of_c/c_tables/main.c
+++ CodeC/finish_the_book_of_c/c_tables/main.c
@@ -7,7 +7,7 @@ void remove_zero_and_tasser(int *tab, int N) {
     /// We fill the table
     for (int i = 0; i < N; i++) {
         printf("Enter element %i : ", i);
-        scanf("%i", tab);
+        scanf("%i", tab[i]);
     }
 }
 /// Function that calculates the sum, product and average of numbers
