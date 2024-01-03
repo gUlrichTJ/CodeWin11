7ccfc96d5b71258bcad30ea13fe86471c2b3c83b
gUlrichTJ <gracianomathey@gmail.com>
2024-01-03 21:00:02 +0000

    Auto commit at

--- AndroidStudioProjects/file_manager/lib/app/features/home/models/user.dart
+++
@@ -1,8 +0,0 @@
-// part of home;
-
-// Create a class named user
-class User {
-  String name = "";
-  // Constructor
-  User({required this.name});
-}
--- AndroidStudioProjects/redoTheCarsProject/README.md
+++
@@ -1,6 +0,0 @@
-# let_s_code_in_flutter5
-
-A new Flutter project.
-
-## Getting Started
-
