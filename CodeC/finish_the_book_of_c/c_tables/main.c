41fb6a512fd8ef7e7d1b0df00b22271aa8821461
gUlrichTJ <gracianomathey@gmail.com>
2024-01-06 10:42:02 +0000

    Auto commit at

--- CodeC/finish_the_book_of_c/c_tables/main.c
+++ CodeC/finish_the_book_of_c/c_tables/main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define new_line putchar('\n');
+#define __new_line putchar('\n');
 
 /// We shall remove all the zeros and tasser the rest of the elements
 void remove_zero_and_tasser(int *tab, int N) {
-    new_line
+    __new_line
 
     /// We fill the table
     for (int i = 0; i < N; i++) {
@@ -13,7 +13,9 @@ void remove_zero_and_tasser(int *tab, int N) {
         scanf("%i", &tab[i]);
     }
 
-    /// We display the table
+    /// We display the table after going to a new line
+    new_line
+    for (int i = 0; i < N; i++)
 }
 /// Function that calculates the sum, product and average of numbers
 void sum_product_average(int N) {
