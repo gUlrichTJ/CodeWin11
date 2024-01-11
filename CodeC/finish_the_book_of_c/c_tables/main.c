bb02c68565ccde7c76ba5f44802348cd334e0af4
gUlrichTJ <gracianomathey@gmail.com>
2024-01-11 09:34:01 +0000

    Auto commit at

--- CodeC/finish_the_book_of_c/c_tables/main.c
+++ CodeC/finish_the_book_of_c/c_tables/main.c
@@ -44,7 +44,7 @@ void display_table_elements(int *tab, int N) {
 }
 
 /// Function to take the number of lines of an array of two dimensions
-int number_of_lines(int *tab) {
+int number_of_lines(int **tab) {
     L = 0;
 
     __new_line
@@ -60,7 +60,7 @@ int number_of_lines(int *tab) {
 }
 
 /// Function to take the number of columns of an array of two dimensions
-int number_of_columns(int *tab) {
+int number_of_columns(int **tab) {
     C = 0;
 
     __new_line
