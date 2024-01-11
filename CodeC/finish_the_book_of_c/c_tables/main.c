9558108460d1d345bd5622aabf33028351389269
gUlrichTJ <gracianomathey@gmail.com>
2024-01-11 13:04:02 +0000

    Auto commit at

--- CodeC/finish_the_book_of_c/c_tables/main.c
+++ CodeC/finish_the_book_of_c/c_tables/main.c
@@ -262,15 +262,51 @@ void sum_product_average(int N) {
 
 int main()
 {
-    int N = 50;
-    int L = 20, C = 20;
-    int tab[N];
-    int tab2[L][C];
+    int T[50][50]; /* tableau donné */
+    int L, C; /* dimensions */
+    int I, J; /* indices courants */
+    long SOM; /* somme des éléments - type long à cause */
+     /* de la grandeur prévisible des résultats. */
+    /* Saisie des données */
+    printf("Nombre de lignes (max.50) : ");
+    scanf("%d", &L );
+    printf("Nombre de colonnes (max.50) : ");
+    scanf("%d", &C );
+    for (I=0; I<L; I++)
+        for (J=0; J<C; J++)
+        {
+        printf("Elément[%d][%d] : ",I,J);
+         scanf("%d", &T[I][J]);
+        }
+    /* Affichage du tableau */
+    printf("Tableau donné :\n");
+    for (I=0; I<L; I++)
+     {
+        for (J=0; J<C; J++)
+            printf("%7d", T[I][J]);
+        printf("\n");
+     }
+    /* Calcul et affichage de la somme des lignes */
+    for (I=0; I<L; I++)
+     {
+        for (SOM=0, J=0; J<C; J++)
+            SOM += T[I][J];
+     printf("Somme - ligne %d : %ld\n",I,SOM);
+     }
+    /* Calcul et affichage de la somme des colonnes */
+    for (J=0; J<C; J++)
+     {
+        for (SOM=0, I=0; I<L; I++)
+        SOM += T[I][J];
+        printf("Somme - colonne %d : %ld\n",J,SOM);
+     }
+     return 0;
+
 
     // remove_zero_and_tasser(tab);
     // inverse_table_without_help(tab);
     // positives_vs_negatives(tab);
 
-    sum_of_lines_and_columns(tab2);
-    return 0;
+   // sum_of_lines_and_columns(tab2);
+   // return 0;
 }
