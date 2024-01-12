bc7b7ce2af930a11b48ef2b0fd9ef4e1e56108af
gUlrichTJ <gracianomathey@gmail.com>
2024-01-12 10:24:05 +0000

    Auto commit at

--- CodeC/finish_the_book_of_c/c_tables/main.c
+++ CodeC/finish_the_book_of_c/c_tables/main.c
@@ -393,6 +393,12 @@ void following_ones_counter(int *tab) {
    int max = 0, k = 0;
    for (i = 0; i < N - 1; i++) {
       if (tab[i] == 1) {
+         k++
+         if (max < k) max = k;
+      } else {
+         k = 0;
+      }
+      if (tab[i] == 1) {
          k++;
          if (max < k) max = k;
          if (tab[i + 1] == 0)
@@ -439,4 +445,3 @@ int main()
     two_dim_array_transfert_to_one_dim();
     return 0;
 }
-                                                                                                            
