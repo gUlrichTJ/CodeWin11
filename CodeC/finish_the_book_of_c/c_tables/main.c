c169c7f85d1a427f9f50099fd829071bfdfcf14c
gUlrichTJ <gracianomathey@gmail.com>
2024-01-16 15:40:01 +0000

    Auto commit at

--- CodeC/finish_the_book_of_c/c_tables/main.c
+++ CodeC/finish_the_book_of_c/c_tables/main.c
@@ -441,7 +441,7 @@ void insert_value_in_a_sorted_list(int *tab) {
     display_table_elements(tab, N);
 }
 
-/// Exercice 7.12 Research of a value in a sorted array
+/// Exercice 7.12 Research of a value in a sorted array (binary search)
 
 /// We will test if a number is a prime number of not
 // I'm going to count the number of following ones in the array
