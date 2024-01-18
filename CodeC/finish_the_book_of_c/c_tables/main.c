c8254b9cde04fb0a2d0c18760f7cc77c92e68bdf
gUlrichTJ <gracianomathey@gmail.com>
2024-01-18 09:52:01 +0000

    Auto commit at

--- CodeC/finish_the_book_of_c/c_tables/main.c
+++ CodeC/finish_the_book_of_c/c_tables/main.c
@@ -451,7 +451,9 @@ void values_in_sorted_array(int *tab) {
     display_table_elements(tab, N);
 
     /// We start searching.
-    for ()
+    for (i = 0; i < N; i++) {
+
+    }
 }
 
 /// We will test if a number is a prime number of not
