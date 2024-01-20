c349875669c21a1c401252ba87e79b78fe3d1adb
gUlrichTJ <gracianomathey@gmail.com>
2024-01-20 17:18:02 +0000

    Auto commit at

--- RustProjects/rust_rover_project/src/main.rs
+++ RustProjects/rust_rover_project/src/main.rs
@@ -1,4 +1,11 @@
 fn main() {
     print!("\n\nHello, world!\n\n\n");
-    for
+    for i in 0..10 {
+        println!("Nous verrons. \nRetiens ça ! Nous verrons\n\
+        Quelque soit la façon dont tu parrais compliqué, je t'aurai.\n\
+        Je te le promets.\n\
+        N'oublie que plus une chose est difficile, plus je persiste à \
+        la résoudre. \nTu es donc l'un des langages que je ne comprends pas \
+        facilement, ce qui me pousse à t'apprendre.")
+    }
 }
