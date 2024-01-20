0a9565a773b5ce0664a315e0ec582a7ee01e3b7d
gUlrichTJ <gracianomathey@gmail.com>
2024-01-20 23:26:01 +0000

    Auto commit at

--- RustProjects/codes/rust_rover_project/src/main.rs
+++ RustProjects/codes/rust_rover_project/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
     println!("Hello, world!");
+
+    let mut x = vec!["Hello, World!"];
+    let _y = &x[0];
+    x.push("foo");
+
+    /// To avoid some errors, we can use these methods :
 }
