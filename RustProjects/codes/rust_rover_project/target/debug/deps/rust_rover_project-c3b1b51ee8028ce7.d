9c0d531e7ab2d84ea2e6da0c848f976eb3156761
gUlrichTJ <gracianomathey@gmail.com>
2024-01-20 23:22:06 +0000

    Auto commit at

---
+++ RustProjects/codes/rust_rover_project/target/debug/deps/rust_rover_project-c3b1b51ee8028ce7.d
@@ -0,0 +1,5 @@
+/home/gracianomathey/codes/CodeWin11/RustProjects/codes/rust_rover_project/target/debug/deps/rust_rover_project-c3b1b51ee8028ce7: src/main.rs
+
+/home/gracianomathey/codes/CodeWin11/RustProjects/codes/rust_rover_project/target/debug/deps/rust_rover_project-c3b1b51ee8028ce7.d: src/main.rs
+
+src/main.rs:
---
+++ RustProjects/codes/rust_rover_project/target/debug/rust_rover_project.d
@@ -0,0 +1,1 @@
+/home/gracianomathey/codes/CodeWin11/RustProjects/codes/rust_rover_project/target/debug/rust_rover_project: /home/gracianomathey/codes/CodeWin11/RustProjects/codes/rust_rover_project/src/main.rs
