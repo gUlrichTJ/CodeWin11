f875fbd7c90e54d4f4de12b74f7b636fdc69ba33
gUlrichTJ <gracianomathey@gmail.com>
2024-01-22 12:42:01 +0000

    Auto commit at

--- RustProjects/codes/guessing/src/main.rs
+++ RustProjects/codes/guessing/src/main.rs
@@ -3,4 +3,10 @@ use std::io;
 fn main() {
     println!("GUESSING GAME");
     print!("Please, input your guess : ");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+        .ok()
+        .expect("Failed to read line");
 }
