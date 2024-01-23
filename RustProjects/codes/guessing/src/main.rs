495fe0b001f4a6ed38706b572a0d1b346c5ded3f
gUlrichTJ <gracianomathey@gmail.com>
2024-01-23 07:20:02 +0000

    Auto commit at

--- RustProjects/codes/guessing/src/main.rs
+++ RustProjects/codes/guessing/src/main.rs
@@ -1,23 +1,14 @@
 extern crate rand;
-
 use std::io;
 use rand::Rng;
-
 fn main() {
-    println!("GUESSING GAME");
-    println!("Please, input your guess : ");
-
-    // We generate the secret number
+    println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1, 101);
-
-    // We print the secret number
-    println!("The secret number is {}", secret_number);
-
+    println!("The secret number is: {}", secret_number);
+    print!("Please input your guess.");
     let mut guess = String::new();
-
     io::stdin().read_line(&mut guess)
         .ok()
-        .expect("Failed to read line");
-
-    println!("You guessed {}", guess);
+        .expect("failed to read line");
+    println!("You guessed: {}", guess);
 }
