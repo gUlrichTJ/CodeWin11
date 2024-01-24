7a51795c9e9538720cd5a42aef5635f3d0042a0d
gUlrichTJ <gracianomathey@gmail.com>
2024-01-24 10:26:02 +0000

    Auto commit at

--- RustProjects/codes/guessing/src/main.rs
+++ RustProjects/codes/guessing/src/main.rs
@@ -23,6 +23,8 @@ fn main() {
     println!("You guessed {}", guess);
     
     match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small")
+        Ordering::Less => println!("Too small"),
+        Ordering::Greater => println!("Too big"),
+        Ordering::Equal => println!("You win"),
     }
 }
