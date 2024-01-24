6348b222567a9dd41c5b4bc585243dfced8b018a
gUlrichTJ <gracianomathey@gmail.com>
2024-01-24 10:20:00 +0000

    Auto commit at

--- RustProjects/codes/guessing/src/main.rs
+++ RustProjects/codes/guessing/src/main.rs
@@ -17,10 +17,6 @@ fn main() {
 
     io::stdin().read_line(&mut guess)
         .ok()
-        .expect("For i in range k");
-
-    io::stdin().read_line(&mut guess)
-        .ok()
         .expect("Failed to read line");
 
     println!("You guessed {}", guess);
