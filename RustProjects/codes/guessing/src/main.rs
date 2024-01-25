3985af67b444d72acb853fc0f5e95a217b415087
gUlrichTJ <gracianomathey@gmail.com>
2024-01-25 18:00:02 +0000

    Auto commit at

--- RustProjects/codes/guessing/src/main.rs
+++ RustProjects/codes/guessing/src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     println!("GUESSING GAME");
 
     // Variable that will take the secret number
-    let secret_number: i32 = rand::thread_rng().gen_range(1, 101);
+    let secret_number: u32 = rand::thread_rng().gen_range(1, 101);
 
     println!("The secret number is : {}", secret_number);
 
@@ -31,7 +31,12 @@ fn main() {
         println!("You guessed : {}", guess);
         
         match guess.cmp(&secret_number) {
-            Ordering::Less => 
+            Ordering::Less    => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal   => {
+                println!("You won");
+                break;
+            }
         }
     }
 }
