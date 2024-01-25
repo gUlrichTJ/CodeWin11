32df1f5cea0d7e906f12e32b2b3f79d84d7828f0
gUlrichTJ <gracianomathey@gmail.com>
2024-01-25 17:48:01 +0000

    Auto commit at

--- RustProjects/codes/guessing/src/main.rs
+++ RustProjects/codes/guessing/src/main.rs
@@ -8,5 +8,16 @@ fn main() {
     println!("GUESSING GAME");
 
     // Variable that will take the secret number
-    let secret_number: i32 = 
+    let secret_number: i32 = rand::thread_rng().gen_range(1, 101);
+
+    println!("The secret number is : {}", secret_number);
+
+    loop {
+        print!("Please, input your guess : ");
+
+        // He will print a String
+        let mut guess = String::new();
+
+        
+    }
 }
