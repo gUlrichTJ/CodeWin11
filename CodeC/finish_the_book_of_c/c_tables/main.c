0a336b21b405309a3dacb8b5732779b9e617a28f
gUlrichTJ <gracianomathey@gmail.com>
2024-02-07 15:54:01 +0000

    Auto commit at

--- CodeC/finish_the_book_of_c/c_tables/main.c
+++ CodeC/finish_the_book_of_c/c_tables/main.c
@@ -646,7 +646,9 @@ void statistics(int *points) {
     // We search the maximum mark and the minimum mark
     min_mark = max_mark = points[0];
     for (i = 0; i < N; i++) {
-        min_mark = points[i] ? min_mark > points[i] : C = 0 ;
+        if (min_mark > points[i])
+            min_mark = points[i];
+        min_mark = min_mark > points[i] ? points[i] : points[0];
     }
     // Average of marks
     printf("\nThe average of marks is %lf\n", return_array_element_sum(points, N) / N);
