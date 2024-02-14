ac81d6d03c0dbace7d92f4fab9786faab7c9fefc
gUlrichTJ <gracianomathey@gmail.com>
2024-02-14 14:06:01 +0000

    Auto commit at

--- AndroidStudioProjects/code_book_reading/lib/main.dart
+++ AndroidStudioProjects/code_book_reading/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        primarySwatch: Colors.lightBlue,
+        primarySwatch: Colors.grey,
         canvasColor: Colors.white,
       ),
       // The home
