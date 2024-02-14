f4109a4521bd509b5d0e1234ee468bf2d89d6f18
gUlrichTJ <gracianomathey@gmail.com>
2024-02-14 17:52:01 +0000

    Auto commit at

--- AndroidStudioProjects/code_book_reading/lib/components/my_file.dart
+++ AndroidStudioProjects/code_book_reading/lib/components/my_file.dart
@@ -2,22 +2,24 @@ import 'package:flutter/material.dart';
 
 class MyTextField extends StatelessWidget {
 
-  final controller = 0;
-  final String hintText = "";
-  final bool obscureText = true;
+  final controller;
+  final String hintText;
+  final bool obscureText;
 
   const MyTextField(
       {
-        super.key,
+        Key? key,
         required this.controller,
         required this.hintText,
-      });
+        required this.obscureText,
+      }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 25.0),
       child: TextField(
+        controller: controller,
         cursorColor: Colors.black,
         decoration: InputDecoration(
           enabledBorder: const OutlineInputBorder(
