42b94ff11234b172f429126ff0adf2775edf90bf
gUlrichTJ <gracianomathey@gmail.com>
2024-02-14 17:44:01 +0000

    Auto commit at

--- AndroidStudioProjects/code_book_reading/lib/components/my_file.dart
+++ AndroidStudioProjects/code_book_reading/lib/components/my_file.dart
@@ -1,2 +1,25 @@
 import 'package:flutter/material.dart';
 
+class MyTextField extends StatelessWidget {
+  const MyTextField({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+      child: TextField(
+        cursorColor: Colors.black,
+        decoration: InputDecoration(
+          enabledBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey.shade500),
+          ),
+          fillColor: Colors.grey.shade200,
+          filled: true,
+        ),
+      ),
+    );
+  }
+}
--- AndroidStudioProjects/code_book_reading/lib/login/login.file.dart
+++ AndroidStudioProjects/code_book_reading/lib/login/login.file.dart
@@ -1,3 +1,4 @@
+import 'package:code_book_reading/components/my_file.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -7,50 +8,36 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey.shade400,
-      body: SafeArea(
+      body: const SafeArea(
         child: Center(
           child: Column(
             children: [
-              const SizedBox(height: 50,),
+              SizedBox(height: 50,),
 
               // logo
-              const Icon(
+              Icon(
                   Icons.lock,
                 size: 100,
                 color: Colors.black,
               ),
 
-              const SizedBox(height: 50,),
+              SizedBox(height: 50,),
 
               // welcome back you've been missed !
-              const Text("You are welcome ! You've been missed !",
+              Text("You are welcome ! You've been missed !",
                 style: TextStyle(
                     color: Colors.black,
                   fontSize: 16,
                 ),
               ),
 
-              const SizedBox(height: 50,),
+              SizedBox(height: 50,),
 
             // username textfield
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25.0),
-              child: TextField(
-                cursorColor: Colors.black,
-                decoration: InputDecoration(
-                  enabledBorder: const OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.grey.shade500),
-                  ),
-                  fillColor: Colors.grey.shade200,
-                  filled: true,
-                ),
-              ),
-            ),
+            MyTextField(),
 
             // password textfield
+              MyTextField(),
 
             // forgot password
 
