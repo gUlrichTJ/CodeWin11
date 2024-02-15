9241ccb6255a7f8fe86d4ea33218e7fa97bc3e1d
gUlrichTJ <gracianomathey@gmail.com>
2024-02-15 17:44:02 +0000

    Auto commit at

--- IdeaProjects/java_homeworks/src/exercices/exo1/Exo1HearWindow.java
+++ IdeaProjects/java_homeworks/src/exercices/exo1/Exo1HearWindow.java
@@ -1,7 +1,11 @@
 package exercices.exo1;
 
+import javax.swing.*;
+import java.awt.event.*;
 public class Exo1HearWindow extends JFrame implements MouseListener {
 
+   // Default constructor
+   public Exo1HearWindow() {}
    public Exo1HearWindow() {
       super("Exo1HearWindow");
       this.setSize(400, 400);
