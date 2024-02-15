4749a4e9cda331eda35100ff8f0afdb96b4e0cc8
gUlrichTJ <gracianomathey@gmail.com>
2024-02-15 18:30:01 +0000

    Auto commit at

---
+++ IdeaProjects/java_homeworks/src/exercices/exo2/Exo2HearWindow.java
@@ -0,0 +1,32 @@
+package exercices.exo2;
+
+import javax.swing.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class Exo2HearWindow extends JFrame implements MouseListener {
+   @Override
+   public void mouseClicked(MouseEvent e) {
+
+   }
+
+   @Override
+   public void mousePressed(MouseEvent e) {
+
+   }
+
+   @Override
+   public void mouseReleased(MouseEvent e) {
+
+   }
+
+   @Override
+   public void mouseEntered(MouseEvent e) {
+
+   }
+
+   @Override
+   public void mouseExited(MouseEvent e) {
+
+   }
+}
