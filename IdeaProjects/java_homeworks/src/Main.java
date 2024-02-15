998edc07852aec1ffa034681e4df6463ef14124b
gUlrichTJ <gracianomathey@gmail.com>
2024-02-15 17:56:01 +0000

    Auto commit at

--- IdeaProjects/java_homeworks/src/Main.java
+++ IdeaProjects/java_homeworks/src/Main.java
@@ -1,5 +1,10 @@
+import exercices.exo1.Exo1HearWindow;
+
+import javax.swing.*;
+
 public class Main {
    public static void main(String[] args) {
       System.out.println("Hello world!");
+      SwingUtilities.invokeLater(Exo1HearWindow::new);
    }
 }
