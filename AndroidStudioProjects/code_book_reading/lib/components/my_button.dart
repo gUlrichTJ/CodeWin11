0889cd8642fbe595f330746869ec99600cd5a704
gUlrichTJ <gracianomathey@gmail.com>
2024-02-16 00:22:01 +0000

    Auto commit at

--- AndroidStudioProjects/code_book_reading/lib/components/my_button.dart
+++ AndroidStudioProjects/code_book_reading/lib/components/my_button.dart
@@ -5,20 +5,23 @@ class MyButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15.0),
-      margin: const EdgeInsets.symmetric(horizontal: 25.0),
-      decoration: BoxDecoration(
-        color: Colors.black,
-        borderRadius: BorderRadius.circular(18),
-      ),
-      child: const Center(
-        child: Text(
-            "Sign in",
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-            fontSize: 16,
+    return GestureDetector(
+      onTap: ,
+      child: Container(
+        padding: const EdgeInsets.all(15.0),
+        margin: const EdgeInsets.symmetric(horizontal: 25.0),
+        decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.circular(18),
+        ),
+        child: const Center(
+          child: Text(
+              "Sign in",
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
           ),
         ),
       ),
