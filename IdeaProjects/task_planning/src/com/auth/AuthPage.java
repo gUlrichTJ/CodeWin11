de54b5d805dc8e34af0424d7d77c41bccea8a685
gUlrichTJ <gracianomathey@gmail.com>
2024-02-16 20:36:01 +0000

    Auto commit at

--- IdeaProjects/task_planning/src/com/auth/AuthPage.java
+++ IdeaProjects/task_planning/src/com/auth/AuthPage.java
@@ -1,6 +1,7 @@
 package com.auth;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import javax.swing.border.LineBorder;
 import java.awt.*;
 
@@ -69,5 +70,28 @@ public class AuthPage extends JFrame {
       panel.add(welcome(), BorderLayout.NORTH);
 
       return panel;
+   }
+}
+
+class RoundedBorder implements Border {
+
+   private int radius;
+
+   RoundedBorder(int radius) {
+      this.radius = radius;
+   }
+   @Override
+   public Insets getBorderInsets(Component c) {
+      return new Insets(this.radius+1, this.radius+1, this.radius+2, this.radius);
+   }
+
+   @Override
+   public boolean isBorderOpaque() {
+      return true;
+   }
+
+   @Override
+   public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+      g.drawRoundRect(x,y,width-1,height-1,radius,radius);
    }
 }
