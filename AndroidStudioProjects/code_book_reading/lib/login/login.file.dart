218471546815101cf9af1519b758fd5e0882061d
gUlrichTJ <gracianomathey@gmail.com>
2024-02-16 00:50:01 +0000

    Auto commit at

--- AndroidStudioProjects/code_book_reading/lib/login/login.file.dart
+++ AndroidStudioProjects/code_book_reading/lib/login/login.file.dart
@@ -17,95 +17,94 @@ class LoginPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.grey.shade400,
       body: SafeArea(
-          child: Center(
-            child: Column(
-              children: [
-                const SizedBox(height: 50,),
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(height: 50,),
 
-                // logo
-                const Icon(
-                    Icons.lock,
-                  size: 100,
-                  color: Colors.black,
+              // logo
+              const Icon(
+                  Icons.lock,
+                size: 100,
+                color: Colors.black,
+              ),
+
+              const SizedBox(height: 50,),
+
+              // welcome back you've been missed !
+              const Text("You are welcome ! You've been missed !",
+                style: TextStyle(
+                    color: Colors.black,
+                  fontSize: 16,
                 ),
+              ),
 
-                const SizedBox(height: 50,),
+              const SizedBox(height: 50,),
 
-                // welcome back you've been missed !
-                const Text("You are welcome ! You've been missed !",
-                  style: TextStyle(
-                      color: Colors.black,
-                    fontSize: 16,
-                  ),
-                ),
+              // username textfield
+              MyTextField(
+                controller: usernameController,
+                hintText: "Username",
+                obscureText: false,
+              ),
 
-                const SizedBox(height: 50,),
+              const SizedBox(height: 10,),
 
-                // username textfield
-                MyTextField(
-                  controller: usernameController,
-                  hintText: "Username",
-                  obscureText: false,
-                ),
+                // password textfield
+              MyTextField(
+                controller: passwordController,
+                hintText: "Password",
+                obscureText: true,
+              ),
 
-                const SizedBox(height: 10,),
+              const SizedBox(height: 10,),
 
-                  // password textfield
-                MyTextField(
-                  controller: passwordController,
-                  hintText: "Password",
-                  obscureText: true,
-                ),
-
-                const SizedBox(height: 10,),
-
-                // forgot password
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Text(
-                          "Forgot password ?",
-                        style: TextStyle(color: Colors.grey.shade600),
-                      ),
-                    ],
-                  ),
-                ),
-
-                const SizedBox(height: 25,),
-
-              // sign in button
-              MyButton(onTap: signUserIn,),
-
-              // or continue with
-              const SizedBox(height: 15,),
-
-              Expanded(
+              // forgot password
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
                 child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.end,
                   children: [
-                    Expanded(
-                      child: Divider(
-                        thickness: 0.5,
-                        color: Colors.grey.shade400,
-                      ),
-                    ),
-                    Expanded(
-                      child: Divider(
-                        thickness: 0.5,
-                        color: Colors.grey.shade400,
-                      ),
+                    Text(
+                        "Forgot password ?",
+                      style: TextStyle(color: Colors.grey.shade600),
                     ),
                   ],
                 ),
               ),
-              // google + apple sign in button
 
-              // not a member ? register now
+              const SizedBox(height: 25,),
 
-              ],
+            // sign in button
+            MyButton(onTap: signUserIn,),
+
+            // or continue with
+            const SizedBox(height: 15,),
+
+            Expanded(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: Divider(
+                      thickness: 0.5,
+                      color: Colors.grey.shade400,
+                    ),
+                  ),
+                  Expanded(
+                    child: Divider(
+                      thickness: 0.5,
+                      color: Colors.grey.shade400,
+                    ),
+                  ),
+                ],
+              ),
             ),
+            // google + apple sign in button
+
+            // not a member ? register now
+
+            ],
           ),
         ),
       ),
