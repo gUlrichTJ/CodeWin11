b7b5394f9a0c416d0dbaf528a0075255543b5c65
gUlrichTJ <gracianomathey@gmail.com>
2024-02-16 12:32:01 +0000

    Auto commit at

--- IdeaProjects/task_planning/src/com/auth/AuthPage.java
+++ IdeaProjects/task_planning/src/com/auth/AuthPage.java
@@ -1,2 +1,16 @@
-package com.auth;public class AuthPage {
+package com.auth;
+
+import javax.swing.*;
+
+public class AuthPage extends JFrame {
+
+   // Constructor
+   public AuthPage() {
+      super("Pretty window");
+      this.setSize(500, 300);
+      this.setLocationRelativeTo(null);
+      this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+      this.setVisible(true);
+   }
 }
--- IdeaProjects/task_planning/src/com/auth/Main.java
+++ IdeaProjects/task_planning/src/com/auth/Main.java
@@ -1,7 +1,9 @@
 package com.auth;
 
+import javax.swing.*;
+
 public class Main {
    public static void main(String[] args) {
-      
+      SwingUtilities.invokeLater(AuthPage::new);
    }
 }
