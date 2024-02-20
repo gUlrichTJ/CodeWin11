3cb993cbe86f1610feb62c37ecf9aec35d9c6896
gUlrichTJ <gracianomathey@gmail.com>
2024-02-20 07:38:01 +0000

    Auto commit at

--- IdeaProjects/task_planning/src/com/auth/databases/AuthDB.java
+++ IdeaProjects/task_planning/src/com/auth/databases/AuthDB.java
@@ -1,9 +1,16 @@
 package com.auth.databases;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 public class AuthDB {
 
-   public static boolean authenticate()
+   public static boolean authenticate(
+         String username, String password
+   ) throws SQLException {
+      Connection connection = DBConnection.getConnection();
+      PreparedStatement preparedStatement = connection.prepareStatement(
+            "select password from users when")
+   }
 }
