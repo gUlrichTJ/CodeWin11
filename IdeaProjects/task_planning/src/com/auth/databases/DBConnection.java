69b95bb804c8d2c9637dcea3635d9f2e1772dfc1
gUlrichTJ <gracianomathey@gmail.com>
2024-02-20 03:52:01 +0000

    Auto commit at

--- IdeaProjects/task_planning/src/com/auth/databases/DBConnection.java
+++ IdeaProjects/task_planning/src/com/auth/databases/DBConnection.java
@@ -1,4 +1,11 @@
 package com.auth.databases;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class DBConnection {
+
+   public static Connection getConnection() throws SQLException {
+      
+   }
 }
