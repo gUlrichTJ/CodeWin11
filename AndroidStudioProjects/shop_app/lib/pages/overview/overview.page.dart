e8bbd810e906d2f8edca4624ea976827d72591c9
gUlrichTJ <gracianomathey@gmail.com>
2024-02-20 13:18:02 +0000

    Auto commit at

---
+++ AndroidStudioProjects/shop_app/lib/pages/overview/overview.page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Overview extends StatefulWidget {
+  const Overview({Key? key}) : super(key: key);
+
+  @override
+  State<Overview> createState() => _OverviewState();
+}
+
+class _OverviewState extends State<Overview> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.settings),
+          ),
+        ],
+      ),
+    );
+  }
+}
