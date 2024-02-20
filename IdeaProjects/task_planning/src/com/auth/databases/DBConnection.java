38414bf7b4bbd67efae9368b55a30f3d29f4be6f
gUlrichTJ <gracianomathey@gmail.com>
2024-02-20 03:54:01 +0000

    Auto commit at

--- IdeaProjects/task_planning/src/com/auth/databases/DBConnection.java
+++ IdeaProjects/task_planning/src/com/auth/databases/DBConnection.java
@@ -1,11 +1,14 @@
 package com.auth.databases;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
 
    public static Connection getConnection() throws SQLException {
-      
+      String url = "jdbc:sqlite:authdatabase.db";
+      Connection connection = DriverManager.getConnection(url);
+      return connection;
    }
 }
