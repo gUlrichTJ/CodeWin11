863f20718186a0b0d1b1c9b2d21333e5bfdad736
gUlrichTJ <gracianomathey@gmail.com>
2024-02-20 07:36:01 +0000

    Auto commit at

--- IdeaProjects/task_planning/src/com/auth/databases/AuthDB.java
+++ IdeaProjects/task_planning/src/com/auth/databases/AuthDB.java
@@ -1,4 +1,9 @@
 package com.auth.databases;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 public class AuthDB {
+
+   public static boolean authenticate()
 }
