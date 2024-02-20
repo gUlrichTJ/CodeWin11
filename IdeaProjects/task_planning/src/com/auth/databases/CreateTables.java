0462be5726d07a8d2d67d9a90cc9bd37c05df695
gUlrichTJ <gracianomathey@gmail.com>
2024-02-20 03:58:01 +0000

    Auto commit at

--- IdeaProjects/task_planning/src/com/auth/databases/CreateTables.java
+++ IdeaProjects/task_planning/src/com/auth/databases/CreateTables.java
@@ -1,6 +1,16 @@
 package com.auth.databases;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class CreateTables {
 
-   
+   public static void main(String[] args) throws SQLException {
+      Connection connection = DBConnection.getConnection();
+      Statement statement = connection.createStatement();
+
+      // We create the table
+      String sql = "create table if not exists user(id integer primary key)";
+   }
 }
