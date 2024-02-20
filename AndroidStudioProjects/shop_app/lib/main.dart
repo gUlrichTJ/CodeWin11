75af0aa91830b538cd1ffbe96c3d747e4ed53ffa
gUlrichTJ <gracianomathey@gmail.com>
2024-02-20 01:00:01 +0000

    Auto commit at

--- AndroidStudioProjects/shop_app/lib/main.dart
+++ AndroidStudioProjects/shop_app/lib/main.dart
@@ -78,7 +78,8 @@ class _OurHomePageState extends State<OurHomePage> {
                   width: MediaQuery.of(context).size.width * 0.9,
                   color: Colors.blueAccent,
                   decoration: BoxDecoration(
-
+                    border: Border.all(
+                      color: 4.0,)
                   ),
                   child: TextButton(
                       onPressed: () {},
