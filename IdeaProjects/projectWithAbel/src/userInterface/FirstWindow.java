59ada7ded152f108d6a7db2d09e07a60915ae3e5
gUlrichTJ <gracianomathey@gmail.com>
2024-02-21 21:32:01 +0000

    Auto commit at

--- IdeaProjects/projectWithAbel/src/userInterface/FirstWindow.java
+++ IdeaProjects/projectWithAbel/src/userInterface/FirstWindow.java
@@ -8,7 +8,10 @@ public class FirstWindow extends JFrame {
    public FirstWindow() {
       /**Le titre de la fenêtre*/
       this.setTitle("Fenetre de Abel");
-
+      /**La taille de la fenêtre*/
+      this.setSize(400, 500);
+      /**Faire en sorte que le programme s'arrêtre lorsqu'on clique le bouton fermer.*/
+      this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
       this.setVisible(true);
    }
