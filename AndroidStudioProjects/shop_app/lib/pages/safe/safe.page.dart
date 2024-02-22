855fff1c8c2fd1c717bcff6c02fbbff192ea586c
gUlrichTJ <gracianomathey@gmail.com>
2024-02-22 21:54:01 +0000

    Auto commit at

--- AndroidStudioProjects/shop_app/lib/pages/safe/safe.page.dart
+++ AndroidStudioProjects/shop_app/lib/pages/safe/safe.page.dart
@@ -1,1 +1,16 @@
 import 'package:flutter/material.dart';
+
+class SafeAr extends StatefulWidget {
+  const SafeAr({Key? key}) : super(key: key);
+
+  @override
+  State<SafeAr> createState() => _SafeArState();
+}
+
+class _SafeArState extends State<SafeAr> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child: child);
+  }
+}
+
