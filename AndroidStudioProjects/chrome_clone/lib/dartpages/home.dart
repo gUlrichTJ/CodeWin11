2bea82904e5dd956874968a44f399fccf0f8ee86
gUlrichTJ <gracianomathey@gmail.com>
2024-02-27 20:31:25 +0000

    Commit changes

--- AndroidStudioProjects/chrome_clone/lib/dartpages/home.dart
+++ AndroidStudioProjects/chrome_clone/lib/dartpages/home.dart
@@ -221,7 +221,7 @@ class _MyHomePageState extends State<MyHomePage> {
             // TODO : Retourner l'image de R cliquable
             retourneImageCliquable(
                 context,
-                "https://microbenotes.com/wp-content/uploads/2023/04/Bioinformatics-Python-and-R-Programming-Language.jpg",
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/R_logo.svg/800px-R_logo.svg.png",
                 "Learn the R Programming language for data science like the Python Language.",
                 "Description"
             ),
@@ -239,13 +239,13 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             retourneImageCliquable(
               context,
-              "https://southernstudies.olemiss.edu/media/new-encyclopedia-southern-culture-940x348.jpeg",
+              "https://upload.wikimedia.org/wikipedia/commons/f/f8/Encyclopaedia_Britannica_15_with_2002.jpg",
               "Read encyclopedia of southern culture of 1989",
               "Southern encyclopedia",
             ),
             retourneImageCliquable(
               context,
-              "https://i.pcmag.com/imagery/reviews/05cItXL96l4LE9n02WfDR0h-5.fit_scale.size_1028x578.v1582751026.png",
+              "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/Netflix_%282000%29.svg/1920px-Netflix_%282000%29.svg.png",
               "When you're tired, watch films on netflix for fun and for free. Secured films.",
               "Netflix",
             ),
@@ -257,7 +257,7 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             retourneImageCliquable(
               context,
-              "https://media.licdn.com/dms/image/D4E03AQHLIIxLO3LnLA/profile-displayphoto-shrink_800_800/0/1679348450752?e=1704326400&v=beta&t=igRAI0P4SUyMh4s6EnWK34aNYd6c8XeG4g-qGn_rLKs",
+              "https://media.licdn.com/dms/image/D4E03AQGB92sGdrGA0A/profile-displayphoto-shrink_800_800/0/1704878089813?e=1714608000&v=beta&t=_eOfinCOnxOfqKRhDKr6z8jDZHJyMs-6DaL14dUgeXE",
               "KELE Bernadin, one of the best flutter developers in Togo has got his bachelor degree after three years of study at EPL",
               "KELE Bernadin",
             ),
