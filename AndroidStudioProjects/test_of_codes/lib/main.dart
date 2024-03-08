32fad217f2087bc55ab3b21d7bc06ca5773ac802
gUlrichTJ <gracianomathey@gmail.com>
2024-03-08 01:33:10 +0000

    Commit the changes

--- AndroidStudioProjects/test_of_codes/lib/main.dart
+++ AndroidStudioProjects/test_of_codes/lib/main.dart
@@ -1,43 +1,18 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'package:test_of_codes/tuto_tts/tuto1/pages/home.page.dart';
-=======
-import 'package:test_of_codes/other/pages/dart/home/home.dart';
->>>>>>> refs/remotes/origin/main
 
-void main () {
-  runApp(const MyApp());
+void main() {
+  runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.lightGreen,
-        canvasColor: Colors.lightGreen.shade200,
-      ),
-      title: "Read your books quickly",
-      debugShowCheckedModeBanner: false,
-<<<<<<< HEAD
-      title: "Text To Speech",
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      routes: {
-        "/":(context)=>HomePage2(),
-      },
-=======
-      home: const OurHomePage(),
->>>>>>> refs/remotes/origin/main
+      home: HomePage2(),
     );
   }
 }
+
