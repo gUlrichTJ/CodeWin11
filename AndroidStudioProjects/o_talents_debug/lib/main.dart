c392e83b5cc458ba27b6df4fbbcaf74441c4ff1a
gUlrichTJ <gracianomathey@gmail.com>
2024-03-09 06:54:12 +0000

    Change debugshowcheckedmodebanner to true

--- AndroidStudioProjects/o_talents_debug/lib/main.dart
+++ AndroidStudioProjects/o_talents_debug/lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: true,
+      debugShowCheckedModeBanner: false,
       routes: {
         "/": (context)=>HomePage(),
         "presence":(context)=>PresenceWidget(),
