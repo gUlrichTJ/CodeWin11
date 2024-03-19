a794c4818f80680ee70558f78e74f000a27b5f8f
gUlrichTJ <gracianomathey@gmail.com>
2024-03-19 16:19:39 +0000

    Commit changes

--- AndroidStudioProjects/o_talents/o_talents_debug2/lib/presence/presence.page.dart
+++ AndroidStudioProjects/o_talents/o_talents_debug2/lib/presence/presence.page.dart
@@ -20,6 +20,9 @@ class _PresenceWidgetState extends State<PresenceWidget> {
   /// Variable qui permet de connaître le bouton qui est sélectionné
   bool selection = true;
 
+  /// Variable booléenne pour arrêter la lecture
+  bool arreterLecture = false;
+
   ///
   int onIconButtonTaped = 0;
 
@@ -27,32 +30,32 @@ class _PresenceWidgetState extends State<PresenceWidget> {
   List<PersonnageEleve> personnageEleve = [
     // TODO: Nous sommes en train de mettre la liste des eleves
     PersonnageEleve(
-        "Sophie",
+        "Adzo",
         "Martin",
         "https://randomuser.me/api/portraits/women/72.jpg",
         true,
     ),
     PersonnageEleve(
       "Maxime",
-      "Dupont",
+      "Kouma",
       "https://randomuser.me/api/portraits/men/76.jpg",
       true,
     ),
     PersonnageEleve(
-      "Sophie",
+      "Ablavi",
       "Martinez",
       "https://randomuser.me/api/portraits/women/75.jpg",
       true,
     ),
     PersonnageEleve(
       "James",
-      "Martin",
+      "Agbavon",
       "https://randomuser.me/api/portraits/men/85.jpg",
       true,
     ),
     PersonnageEleve(
       "Kilyan",
-      "Martin",
+      "Israéliotte",
       "https://randomuser.me/api/portraits/men/5.jpg",
       true,
     ),
@@ -64,15 +67,15 @@ class _PresenceWidgetState extends State<PresenceWidget> {
     ),
     PersonnageEleve(
       "Loly",
-      "Martinez",
+      "Afatono",
       "https://randomuser.me/api/portraits/women/77.jpg",
-      true,
+      false,
     ),
     PersonnageEleve(
-      "Jacques",
+      "Davi",
       "Françoise",
       "https://randomuser.me/api/portraits/women/37.jpg",
-      true,
+      false,
     ),
   ];
   @override
@@ -99,45 +102,37 @@ class _PresenceWidgetState extends State<PresenceWidget> {
               setState(() {
                 ++onIconButtonTaped;
                 debugPrint("$onIconButtonTaped");
+                arreterLecture = !arreterLecture;
               });
             },
             /// Je vais essayer la fonctionalité stop.
             child: IconButton(
               tooltip: "Lire les noms.",
               onPressed: () async {
-                /* if (selection == true) {
-                   for (var person in personnageEleve) {
-                     if (selection == true) {
-                         /// Maintenant, il me faut l'appeler 4 fois.
-                         for (int i = 0; i < nombreDAppelsEleve; i++) {
-                           if (selection == true) {
-                             /// TODO: Je dois ici appeler la fonction speak de la classe TextToSpeecManager
-                             textToSpeechManager.speak(
-                                 '${person.nom} ${person.prenom}'
-                             );
-                             await Future.delayed(const Duration(seconds: 2));
+                setState(() {
+                  selection = !selection;
+                });
+                if (selection != true) {
 
-                             /// Nous incrémentons la varialble qui compte le nombre de fois que
-                             /// le nom de l'élève a été appelé.
-                             ++compteurDAppels;
-                           } else {
-                             textToSpeechManager.stop();
-                           }
-                         }
-                        } else {
-                          textToSpeechManager.stop();
-                        }
-                      }
+                  for (var person in personnageEleve) {
+                    if (selection != true) {
+                      await textToSpeechManager.speak(
+                          "${person.nom} ${person.prenom}"
+                      );
+                      await Future.delayed(const Duration(seconds: 2));
                     } else {
-                    textToSpeechManager.stop();
-                 } */
-                
+                      textToSpeechManager.stop();
+                      break;
+                    }
+                  }
+                } else {
+                  textToSpeechManager.stop();
+                }
 
                 /// Nous changeons d'état pour changer l'icône
                  setState(() {
                     selection = !selection;
                   });
-                  // Navigator.of(context).pushNamed("test_lecture");
                 },
                 icon: selection ?
                 const Icon(Icons.volume_up) :
