45b154dbe7b8274ac88abd0cf91ff93da2c2e30b
gUlrichTJ <gracianomathey@gmail.com>
2024-04-02 17:01:50 +0000

    add the code of the github_mobile_app

---
+++ AndroidStudioProjects/yousfi_codes/github_mobile_app/lib/pages/repositories/home.page.dart
@@ -0,0 +1,83 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class GitRepositoriesPage extends StatefulWidget {
+
+  String login;
+
+  /// L'image de la personne
+  String avatarUrl;
+  GitRepositoriesPage(this.login, this.avatarUrl, {super.key});
+
+  @override
+  State<GitRepositoriesPage> createState() => _GitRepositoriesPageState();
+}
+
+class _GitRepositoriesPageState extends State<GitRepositoriesPage> {
+
+  /// Récupérons les données
+  dynamic dataRepositories;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadRepositories();
+  }
+
+  /// J'appelle cette méthode jusuqu'à ce que j'aie la réponse.
+  void loadRepositories() async {
+    String url = "https://api.github.com/users/${widget.login}/repos";
+    Uri urlUri = Uri.parse(url);
+    http.Response response = await http.get(urlUri);
+    if (response.statusCode == 200) {
+      setState(() {
+        dataRepositories = json.decode(response.body);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      /// Le appbar
+      appBar: AppBar(
+        title: Text("Repositories ${widget.login}"),
+        /// The button of returning to the other page
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: const Icon(Icons.arrow_back_outlined),
+        ),
+        /// Nous allons mettre l'image de l'user here
+        actions: [
+          CircleAvatar(
+            child: Image.network("${dataRepositories['avatar_url']}"),
+          )
+        ],
+        /// The background color of the appbar
+        backgroundColor: Colors.grey.shade500,
+      ),
+      /// The background color of the whole app
+      backgroundColor: Colors.grey.shade400,
+      body: Center(
+        child: ListView.separated(
+            itemBuilder: (context, index) => ListTile(
+              /// Le nom du repo
+              title: Text("${dataRepositories[index]['name']}"),
+            ),
+            separatorBuilder: (context, index) => Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Divider(height: 2,
+                color: Colors.grey.shade600,
+              ),
+            ),
+            itemCount: dataRepositories == null ? 0 : dataRepositories.length,
+        ),
+      ),
+    );
+  }
+}
--- AndroidStudioProjects/yousfi_codes/github_mobile_app/lib/pages/users/users.page.dart
+++ AndroidStudioProjects/yousfi_codes/github_mobile_app/lib/pages/users/users.page.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:github_mobile_app/pages/repositories/home.page.dart';
 import 'package:http/http.dart' as http;
 
 class UsersPage extends StatefulWidget {
@@ -16,33 +19,84 @@ class _UsersPageState extends State<UsersPage> {
 
   TextEditingController queryTextEditingController = TextEditingController();
 
+  /// Il va falloir stocker les données dans un objet de type dart
+  dynamic data = null;
+
   /// La variable qui va permettre de rendre le texte visible ou non
   bool notVisible = false;
 
   /// La variable qui va recevoir le texte
   String query = "";
 
+  /// La page actuelle
+  int currentPage = 0;
+
+  /// Le nombre total de pages
+  int totalPages = 0;
+
+  /// La taille de la page
+  int pageSize = 20;
+
+  /// Pour controller le scrolling de la liste
+  ScrollController scrollController = ScrollController();
+
+  /// Nous perdons les anciennes données parce qu'elles sont stockées dans data
+  /// Nous voulons retrouver nos anciennes données
+  List<dynamic> items = [];
+
   /// La méthode qui va permettre de faire la recherche
   void search(String query) {
-    String url = "https://api.github.com/search/users?q=$query&per_page=20&page=0";
+    String url = "https://api.github.com/search/users?q=$query&per_page=$pageSize&page=$currentPage";
 
+    Uri uriUrl = Uri.parse(url);
     debugPrint(url);
 
-    http.get(url as Uri)
-        .then((value)  {
-
+    /// Nous recueillons l'url
+    http.get(uriUrl)
+        .then((response)  {
+          /// Nous allons décoder les données json
+      /// Nous prenons les données qui sont au format texte et nous les stockons
+      /// dans un objet de type dart
+      /// Nous devons faire ceci dans le méthode setstate pour pouvoir voir
+      /// les modifications dans la page
+        setState(() {
+          data = json.decode(response.body);
+          items.addAll(data['items']);
+          if (data['total_count'] % pageSize == 0) {
+            totalPages = data['total_count'] ~/ pageSize;
+          } else {
+            totalPages = (data['total_count'] / pageSize).floor() + 1;
+          }
+        });
     })
     .catchError((err) {
       debugPrint(err);
     });
   }
 
+  /// Pour initialiser les données, nous allons utiliser initstate
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    scrollController.addListener(() {
+      if (scrollController.position.pixels == scrollController.position.maxScrollExtent) {
+        setState(() {
+          if (currentPage < totalPages - 1) {
+            ++currentPage;
+            /// je recharge les données
+            search(query);
+          }
+        });
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Users => $query"),
+        title: Text("Users => $query => $currentPage / $totalPages"),
         /// The icon that will permit us to return to the home page
         leading: IconButton(
           onPressed: () {
@@ -55,8 +109,8 @@ class _UsersPageState extends State<UsersPage> {
         backgroundColor: Colors.grey.shade500,
       ),
       /// The background of all the app
-  
-  backgroundColor: Colors.grey.shade400,
+
+      backgroundColor: Colors.grey.shade400,
       body: Center(
         child: Column(
           children: [
@@ -102,6 +156,8 @@ class _UsersPageState extends State<UsersPage> {
                 IconButton(
                   onPressed: () {
                     setState(() {
+                      items = [];
+                      currentPage = 0;
                       query = queryTextEditingController.text;
                       search(query);
                       debugPrint(query);
@@ -113,11 +169,64 @@ class _UsersPageState extends State<UsersPage> {
                   ),
                 )
               ],
+            ),
+            /// Pour pouvoir les afficher dans un column, il faut les envelopper
+            /// dans un element expanded
+            Expanded(
+              child: ListView.separated(
+                /// Pour separer
+                separatorBuilder: (context, index) => Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Divider(
+                    height: 2,
+                    thickness: 2,
+                    color: Colors.grey.shade700,
+                  ),
+                ),
+                /// Le controller du scrolling
+                controller: scrollController,
+                /// Le nombre d'éléments de la liste
+                itemCount: items.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    onTap: () {
+                      Navigator.push(context,
+                        MaterialPageRoute(
+                          builder: (context) =>
+                            GitRepositoriesPage(
+                                items[index]['login'],
+                              items[index]['login'],
+                            ),
+                        ),
+                      );
+                    },
+                    title: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            CircleAvatar(
+                              backgroundImage: NetworkImage(
+                                items[index]['avatar_url'],
+                              ),
+                              radius: 40,
+                            ),
+                            const SizedBox(width: 20,),
+                            Text("${items[index]['login']}"),
+                          ],
+                        ),
+                        CircleAvatar(
+                          child: Text("${items[index]['score']}"),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
             )
           ],
         ),
       ),
     );
   }
-
 }
