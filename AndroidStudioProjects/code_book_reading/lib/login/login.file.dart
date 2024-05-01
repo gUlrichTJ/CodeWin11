151132c33c1b1d5ab18a2d377a12c61e55f722a4
gulrichtj <gracianomathey@gmail.com>
2024-05-01 14:39:36 +0000

    login.file.dart change

--- AndroidStudioProjects/code_book_reading/lib/login/login.file.dart
+++ AndroidStudioProjects/code_book_reading/lib/login/login.file.dart
@@ -16,123 +16,125 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey.shade400,
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            children: [
-              const SizedBox(height: 50,),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Center(
+            child: Column(
+              children: [
+                const SizedBox(height: 50,),
 
-              // logo
-              const Icon(
-                  Icons.lock,
-                size: 100,
-                color: Colors.black,
-              ),
-
-              const SizedBox(height: 50,),
-
-              // welcome back you've been missed !
-              const Text("You are welcome ! You've been missed !",
-                style: TextStyle(
-                    color: Colors.black,
-                  fontSize: 16,
+                // logo
+                const Icon(
+                    Icons.lock,
+                  size: 100,
+                  color: Colors.black,
                 ),
-              ),
 
-              const SizedBox(height: 50,),
+                const SizedBox(height: 50,),
 
-              // username textfield
-              MyTextField(
-                controller: usernameController,
-                hintText: "Username",
-                obscureText: false,
-              ),
+                // welcome back you've been missed !
+                const Text("You are welcome ! You've been missed !",
+                  style: TextStyle(
+                      color: Colors.black,
+                    fontSize: 16,
+                  ),
+                ),
 
-              const SizedBox(height: 10,),
+                const SizedBox(height: 50,),
 
-                // password textfield
-              MyTextField(
-                controller: passwordController,
-                hintText: "Password",
-                obscureText: true,
-              ),
+                // username textfield
+                MyTextField(
+                  controller: usernameController,
+                  hintText: "Username",
+                  obscureText: false,
+                ),
 
-              const SizedBox(height: 10,),
+                const SizedBox(height: 10,),
 
-              // forgot password
+                  // password textfield
+                MyTextField(
+                  controller: passwordController,
+                  hintText: "Password",
+                  obscureText: true,
+                ),
+
+                const SizedBox(height: 10,),
+
+                // forgot password
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                          "Forgot password ?",
+                        style: TextStyle(color: Colors.grey.shade600),
+                      ),
+                    ],
+                  ),
+                ),
+
+                const SizedBox(height: 25,),
+
+              // sign in button
+              MyButton(onTap: signUserIn,),
+
+              // or continue with
+              const SizedBox(height: 15,),
+
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 25.0),
                 child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Text(
-                        "Forgot password ?",
-                      style: TextStyle(color: Colors.grey.shade600),
-                    ),
-                  ],
-                ),
-              ),
-
-              const SizedBox(height: 25,),
-
-            // sign in button
-            MyButton(onTap: signUserIn,),
-
-            // or continue with
-            const SizedBox(height: 15,),
-
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25.0),
-              child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Expanded(
-                      child: Divider(
-                        thickness: 0.5,
-                        color: Colors.grey.shade400,
-                      ),
-                    ),
-
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                      child: Text(
-                          "or sign with",
-                        style: TextStyle(
-                          color: Colors.grey.shade700,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Expanded(
+                        child: Divider(
+                          thickness: 0.5,
+                          color: Colors.grey.shade400,
                         ),
                       ),
-                    ),
 
-                    Expanded(
-                      child: Divider(
-                        thickness: 0.5,
-                        color: Colors.grey.shade400,
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                        child: Text(
+                            "or sign with",
+                          style: TextStyle(
+                            color: Colors.grey.shade700,
+                          ),
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-            ),
-            // google + apple sign in button
-            Row(
-              children: [
-                ClipRRect(
-                  borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(8.4),
-                    topRight: Radius.circular(8.4),
-                  ),
 
-                    child: Image.asset(
-                        "assets/images/google_image.png",
-                      width: 20,
-                        height: 20,
-                    )
-                ),
+                      Expanded(
+                        child: Divider(
+                          thickness: 0.5,
+                          color: Colors.grey.shade400,
+                        ),
+                      ),
+                    ],
+                  ),
+              ),
+              // google + apple sign in button
+              Row(
+                children: [
+                  ClipRRect(
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(8.4),
+                      topRight: Radius.circular(8.4),
+                    ),
+
+                      child: Image.asset(
+                          "assets/images/google_image.png",
+                        width: 20,
+                          height: 20,
+                      )
+                  ),
+                ],
+              ),
+
+              // not a member ? register now
+
               ],
             ),
-
-            // not a member ? register now
-
-            ],
           ),
         ),
       ),
